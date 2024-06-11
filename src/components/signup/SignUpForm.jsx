a5d27796a6fa15d47b0fcf6ca0bddc3bbfd961ae
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-11 23:36:05 +0100

    Updated

--- src/components/signup/SignUpForm.jsx
+++ src/components/signup/SignUpForm.jsx
@@ -11,7 +11,7 @@ const SignUpForm = () => {
 
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  const [regIsSuccessful, setRegIsSuccessful] = useState(false);
+  const [regIsSuccessful, setRegIsSuccessful] = useState(true);
 
   const [pswdVisible, setPswdVisible] = useState(false);
   function togglePswdVisible() {
@@ -100,106 +100,120 @@ const SignUpForm = () => {
 
   return (
     <>
-      <form onSubmit={createUser}>
-        {/* name */}
-        <p className="my-2 text-red-500 font-semibold">{error}</p>
-        <div className="mb-3">
-          <div>
-            <label htmlFor="name" className="font-bold text-md">
-              Your Name
-            </label>
+      {regIsSuccessful ? (
+        <>
+          <div className="py-5">
+            <div className="alert alert-success text-white">
+              <p>Your account has been created successfully</p>
+            </div>
+            <p>Logging in...</p>
           </div>
-          <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
-            <i className="fi fi-ts-circle-user flaticon-offset"></i>
-            <input
-              type="text"
-              name="name"
-              ref={nameRef}
-              required
-              className="grow"
-              placeholder="Enter Your Name"
-            />
-          </div>
-        </div>
-        {/* email */}
-        <div className="mb-3">
-          <div>
-            <label htmlFor="email" className="font-bold text-md">
-              Your Email
-            </label>
-          </div>
-          <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
-            <i className="fi fi-tr-envelopes flaticon-offset"></i>
-            <input
-              type="email"
-              name="email"
-              required
-              ref={emailRef}
-              className="grow"
-              placeholder="Enter Your Email"
-            />
-          </div>
-        </div>
-        {/* username */}
-        <div className="mb-3">
-          <div>
-            <label htmlFor="username" className="font-bold text-md">
-              Your Username
-            </label>
-          </div>
-          <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
-            <i className="fi fi-tr-at flaticon-offset"></i>
-            <input
-              type="text"
-              name="username"
-              required
-              ref={usernameRef}
-              className="grow"
-              placeholder="Enter Your Username"
-            />
-          </div>
-        </div>
-        {/* password */}
-        <div className="mb-3">
-          <div>
-            <label htmlFor="password" className="font-bold text-md">
-              Your Password
-            </label>
-          </div>
-          <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
-            <i className="fi fi-tr-padlock-check flaticon-offset"></i>
-            <input
-              type={pswdVisible ? "text" : "password"}
-              name="password"
-              required
-              ref={passwordRef}
-              className="grow"
-              placeholder="Enter Your password"
-            />
-            <i
-              onClick={togglePswdVisible}
-              className={`${
-                pswdVisible ? "fi fi-rr-eye-crossed" : "fi fi-rr-eye"
-              } cursor-pointer flaticon-offset`}
-            ></i>
-          </div>
-        </div>
-        {/* submit button */}
-        <div>
-          <button
-            className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
-            type="submit"
-          >
-            {loading ? (
-              <>
-                <span className="loading loading-dots loading-xs"></span>
-              </>
-            ) : (
-              "Get Started"
-            )}
-          </button>
-        </div>
-      </form>
+        </>
+      ) : (
+        <>
+          <form onSubmit={createUser}>
+            {/* name */}
+            <p className="my-2 text-red-500 font-semibold">{error}</p>
+            <div className="mb-3">
+              <div>
+                <label htmlFor="name" className="font-bold text-md">
+                  Your Name
+                </label>
+              </div>
+              <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
+                <i className="fi fi-ts-circle-user flaticon-offset"></i>
+                <input
+                  type="text"
+                  name="name"
+                  ref={nameRef}
+                  required
+                  className="grow"
+                  placeholder="Enter Your Name"
+                />
+              </div>
+            </div>
+            {/* email */}
+            <div className="mb-3">
+              <div>
+                <label htmlFor="email" className="font-bold text-md">
+                  Your Email
+                </label>
+              </div>
+              <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
+                <i className="fi fi-tr-envelopes flaticon-offset"></i>
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  ref={emailRef}
+                  className="grow"
+                  placeholder="Enter Your Email"
+                />
+              </div>
+            </div>
+            {/* username */}
+            <div className="mb-3">
+              <div>
+                <label htmlFor="username" className="font-bold text-md">
+                  Your Username
+                </label>
+              </div>
+              <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
+                <i className="fi fi-tr-at flaticon-offset"></i>
+                <input
+                  type="text"
+                  name="username"
+                  required
+                  ref={usernameRef}
+                  className="grow"
+                  placeholder="Enter Your Username"
+                />
+              </div>
+            </div>
+            {/* password */}
+            <div className="mb-3">
+              <div>
+                <label htmlFor="password" className="font-bold text-md">
+                  Your Password
+                </label>
+              </div>
+              <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
+                <i className="fi fi-tr-padlock-check flaticon-offset"></i>
+                <input
+                  type={pswdVisible ? "text" : "password"}
+                  name="password"
+                  required
+                  ref={passwordRef}
+                  className="grow"
+                  placeholder="Enter Your password"
+                />
+                <i
+                  onClick={togglePswdVisible}
+                  className={`${
+                    pswdVisible ? "fi fi-rr-eye-crossed" : "fi fi-rr-eye"
+                  } cursor-pointer flaticon-offset`}
+                ></i>
+              </div>
+            </div>
+            {/* submit button */}
+            <div>
+              <button
+                className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <span className="loading loading-dots loading-xs"></span>
+                  </>
+                ) : (
+                  "Get Started"
+                )}
+              </button>
+            </div>
+          </form>
+        </>
+      )}
     </>
   );
 };
