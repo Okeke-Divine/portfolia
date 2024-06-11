08e2701c953bba9b52194bb7ac9b4995ef9f4898
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 00:55:49 +0100

    Updated

--- src/components/shared/ApplicationSidebar.jsx
+++ src/components/shared/ApplicationSidebar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { signOut } from "next-auth/react";
 import Swal from "sweetalert2";
+import config from "@/data/config.json"
 
 export default function ApplicationSidedar() {
   function confirmSignOut() {
@@ -21,6 +22,9 @@ export default function ApplicationSidedar() {
   return (
     <>
       <div className="fixed top-0 left-0 h-[100%] bg-black w-[250px] flex justify-between flex-col text-white p-5">
+        <div className="text-3xl font-bold uppercase">
+            {config.app_name}
+        </div>
         <div className="">
           <button className="btn text-white btn-ghost" onClick={confirmSignOut}>
             <i className="fi fi-tr-sign-out-alt flaticon-offset"></i> Logout
