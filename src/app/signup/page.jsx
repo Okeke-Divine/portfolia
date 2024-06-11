95b9b2345f6306ddc72361a20c432e0cd0bbccd7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-11 20:17:30 +0100

    Updated

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,16 @@
+export const metadata = {
+    title: "Create an Account"
+}
+
+export default function SignUp(){
+    return (
+        <>
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        </>
+    )
+}
