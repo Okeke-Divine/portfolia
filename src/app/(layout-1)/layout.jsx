686bc9a3ce7dbe6d34cd6e01fcf6e906aafbaf8f
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 00:33:14 +0100

    Updated

--- src/app/(layout-1)/layout.jsx
+++ src/app/(layout-1)/layout.jsx
@@ -1,9 +1,15 @@
+import ApplicationSidedar from "@/components/shared/ApplicationSidebar";
 import React from "react";
 
 const Layout1 = ({ children }) => {
   return (
     <>
-      <div>{children}</div>
+      <div>
+        <div>
+          <ApplicationSidedar />
+        </div>
+        <div className="pl-[250px] bg-red-200 w-full">{children}</div>
+      </div>
     </>
   );
 };
---
+++ src/components/shared/ApplicationSidebar.jsx
@@ -0,0 +1,7 @@
+export default function ApplicationSidedar(){
+    return (
+        <>
+        <div className="fixed top-0 left-0 h-[100%] bg-black w-[250px]"></div>
+        </>
+    )
+}
