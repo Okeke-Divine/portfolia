cb7846486616868c3eff506cbc5f35f97bd78ebc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 00:59:50 +0100

    Updated

--- src/components/shared/ApplicationSidebar.jsx
+++ src/components/shared/ApplicationSidebar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { signOut } from "next-auth/react";
 import Swal from "sweetalert2";
-import config from "@/data/config.json"
+import config from "@/data/config.json";
 
 export default function ApplicationSidedar() {
   function confirmSignOut() {
@@ -21,15 +21,21 @@ export default function ApplicationSidedar() {
 
   return (
     <>
-      <div className="fixed top-0 left-0 h-[100%] bg-black w-[250px] flex justify-between flex-col text-white p-5">
+      <div className="fixed top-0 left-0 h-[100%] max-h-[100vh] overflow-y-auto bg-black w-[250px] flex justify-between flex-col text-white p-5">
         <div className="text-2xl text-center font-bold uppercase">
-            {config.app_name}
+          {config.app_name}
         </div>
         <div>
-            
+          <button className="btn btn-ghost w-full mb-2">
+            Analytics
+            <span className="badge badge-info badge-sm">coming soon</span>
+          </button>
+          <button className="btn btn-ghost w-full mb-2">
+            Settings
+          </button>
         </div>
         <div className="">
-          <button className="btn text-white btn-ghost" onClick={confirmSignOut}>
+          <button className="w-full btn text-white btn-ghost" onClick={confirmSignOut}>
             <i className="fi fi-tr-sign-out-alt flaticon-offset"></i> Logout
           </button>
         </div>
