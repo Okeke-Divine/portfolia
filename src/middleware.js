3fc91ed1fa23948ad261a71a87b98e681d2164ad
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 00:28:44 +0100

    Added authetication

--- src/middleware.js
+++ src/middleware.js
@@ -5,21 +5,22 @@ import { getToken } from 'next-auth/jwt';
 const secret = process.env.NEXTAUTH_SECRET;
 
 export async function middleware(req) {
-  // Get the token from the request
-  const token = await getToken({ req, secret });
+    // Get the token from the request
+    const token = await getToken({ req, secret });
+    console.log(token);
 
-  // If no token is found, redirect to the login page
-  if (!token) {
-    const loginUrl = new URL('/api/auth/signin', req.url);
-    loginUrl.searchParams.set('callbackUrl', req.url);
-    return NextResponse.redirect(loginUrl);
-  }
+    // If no token is found, redirect to the login page
+    if (!token) {
+        const loginUrl = new URL('/api/auth/signin', req.url);
+        loginUrl.searchParams.set('callbackUrl', req.url);
+        return NextResponse.redirect(loginUrl);
+    }
 
-  // Continue to the requested page
-  return NextResponse.next();
+    // Continue to the requested page
+    return NextResponse.next();
 }
 
 // Specify the routes for which this middleware should run
 export const config = {
-  matcher: ['/dashboard/'],
+    matcher: ['/dashboard/'],
 };
