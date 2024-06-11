61b51d7f2e38fc62d117a95c007f4d9b95de6b48
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-11 21:08:21 +0100

    Updated

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -14,7 +14,7 @@ export default function SignUp() {
       <div>
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div className="min-h-[100vh] flex items-center justify-center bg-gray-100">
-            <div className="bg-white shadow-sm px-5 py-10 md:px-[50px] md:py-[50px]  rounded-md">
+            <div className="bg-white shadow-md hover:shadow-lg duration-100 px-5 py-10 md:px-[50px] md:py-[50px]  rounded-md">
               <h1>
                 Hi, Welcome to{" "}
                 <Link href="/" className="app-text-primary">
