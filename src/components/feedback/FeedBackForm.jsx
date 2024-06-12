eb96025692b69a80782831fa143e6936adde16b3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 10:56:23 +0100

    Updated

--- src/components/feedback/FeedBackForm.jsx
+++ src/components/feedback/FeedBackForm.jsx
@@ -7,8 +7,24 @@ const FeedBackForm = () => {
         <h3 className="font-bold text-xl">
           We read them all! How can we improve your experience?
         </h3>
+        {/* rating */}
+        <div className="my-2">
+          <div className="rating rating-lg">
+            <input type="radio" name="rating-9" className="rating-hidden" />
+            <input type="radio" name="rating-9" className="mask mask-star-2" />
+            <input type="radio" name="rating-9" className="mask mask-star-2" />
+            <input type="radio" name="rating-9" className="mask mask-star-2" />
+            <input
+              type="radio"
+              name="rating-9"
+              className="mask mask-star-2"
+              defaultChecked={true}
+            />
+            <input type="radio" name="rating-9" className="mask mask-star-2" />
+          </div>
+        </div>
         {/* message */}
-        <div className="mb-2">
+        <div className="my-2">
           <label htmlFor="msg" className="font-semibold">
             Message
           </label>
