d692429125340069c3588846340b007d4ea2996c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 12:33:22 +0100

    Major updated

--- src/components/skeleton/dashboard/DashboardTopSkeleton.jsx
+++ src/components/skeleton/dashboard/DashboardTopSkeleton.jsx
@@ -15,7 +15,7 @@ const DashboardTopSkeleton = () => {
         </div>
         <div className="divider"></div>
         <div>
-          <div className="skeleton h-4 w-32 mb-2"></div>
+          <div className="skeleton h-4 w-1/4 mb-2"></div>
           <div className="skeleton h-32 w-full"></div>
         </div>
       </div>
