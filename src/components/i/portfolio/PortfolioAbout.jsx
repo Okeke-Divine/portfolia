0828eb83c507c09dc3b53364b0978863a12cb88d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:33:08 +0100

    Updated

--- src/components/i/portfolio/PortfolioAbout.jsx
+++ src/components/i/portfolio/PortfolioAbout.jsx
@@ -9,7 +9,23 @@ const PortfolioAbout = () => {
             Nice to meet you.
           </div>
           <div>
-            <h1 className="max-w-[80%] md:max-w-[600px] text-4xl">Hey, I'm John - Full Stack Web Developer.</h1>
+            <h1 className="max-w-[80%] md:max-w-[600px] text-4xl">
+              Hey, I'm John - Full Stack Web Developer.
+            </h1>
+          </div>
+          <div className="mt-2 app-text-light-2 app-portfolio-about-container">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+              beatae consequatur nisi obcaecati voluptas modi laudantium quo,
+              maiores cum ipsam quam nostrum nulla neque, rerum dicta tempora
+              fugit, possimus illo.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+              beatae consequatur nisi obcaecati voluptas modi laudantium quo,
+              maiores cum ipsam quam nostrum nulla neque, rerum dicta tempora
+              fugit, possimus illo.
+            </p>
           </div>
         </div>
       </section>
