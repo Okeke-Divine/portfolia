b915fadb696519f128cbbbef4d9f5b81f32700eb
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:06:37 +0100

    Updated

--- src/components/i/portfolio/PortfolioHero.jsx
+++ src/components/i/portfolio/PortfolioHero.jsx
@@ -4,8 +4,11 @@ const PortfolioHero = () => {
   return (
     <>
       <div>
-        <div className="flex p-5 md:p-10">
-            .
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-5 md:p-10">
+                <div>
+                    <h1>Hey, I'm a full stack web developer</h1>
+                </div>
+                <div></div>
         </div>
       </div>
     </>
