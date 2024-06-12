aa1a3d01a7b79acbfab461281651fd9172d34a91
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 01:23:51 +0100

    Updated

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -9,7 +9,7 @@ const SignIn = () => {
     return (
         <>
           <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 mpx-5">
+            <div className="grid grid-cols-1 md:grid-cols-2">
               <div className="min-h-[100vh] flex items-center justify-center bg-gray-100">
                 <div className="bg-white shadow-md hover:shadow-lg duration-100 px-5 py-10 md:px-[50px] md:py-[50px]  rounded-md">
                   <h1>
