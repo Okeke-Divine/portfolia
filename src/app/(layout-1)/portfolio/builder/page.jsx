be3fa7a688223f03344e7968fb238207cb72f597
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 14:32:12 +0100

    Updated

--- src/app/(layout-1)/portfolio/builder/page.jsx
+++ src/app/(layout-1)/portfolio/builder/page.jsx
@@ -6,20 +6,24 @@ const PortfolioBuilder = async () => {
   return (
     <>
       <div>
-        <h1 className="flex items-center gap-2">
-          <i className="fi fi-ts-web-design titleIcon"></i> Portfolio Builder
-        </h1>
-        <p className="app-text-light">
-          No code Portfolio builder |{" "}
-          <Link
-            href={"/i/" + session.user.username}
-            target="_blanks"
-            className="app-link-primary font-bold"
-          >
-            Live Preview
-            <i className="fi fi-tr-arrow-up-right-from-square text-[15px] pl-1"></i>
-          </Link>
-        </p>
+        <div>
+          <h1 className="flex items-center gap-2">
+            <i className="fi fi-ts-web-design titleIcon"></i> Portfolio Builder
+          </h1>
+          <p className="app-text-light">
+            No code Portfolio builder |{" "}
+            <Link
+              href={"/i/" + session.user.username}
+              target="_blanks"
+              className="app-link-primary font-bold"
+            >
+              Live Preview
+              <i className="fi fi-tr-arrow-up-right-from-square text-[15px] pl-1"></i>
+            </Link>
+          </p>
+        </div>
+        <div className="divider"></div>
+        <div>...</div>
       </div>
     </>
   );
