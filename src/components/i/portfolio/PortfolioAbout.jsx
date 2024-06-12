befdd74bff6d71f1e2715c12672d6bc1184a37e1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:31:53 +0100

    Updated

--- src/components/i/portfolio/PortfolioAbout.jsx
+++ src/components/i/portfolio/PortfolioAbout.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const PortfolioAbout = () => {
   return (
     <>
-        <section className="app-portfolio-padding bg-base-100" id="about">
-            <div>
-                <div className="app-text-light-2 uppercase text-sm font-semibold">Nice to meet you.</div>
-            </div>
-        </section>
+      <section className="app-portfolio-padding bg-base-100" id="about">
+        <div>
+          <div className="app-text-light-2 uppercase text-sm font-semibold">
+            Nice to meet you.
+          </div>
+          <div>
+            <h1 className="max-w-[600px] text-4xl">Hey, I'm John - Full Stack Web Developer.</h1>
+          </div>
+        </div>
+      </section>
     </>
-    )
-}
+  );
+};
 
-export default PortfolioAbout
+export default PortfolioAbout;
