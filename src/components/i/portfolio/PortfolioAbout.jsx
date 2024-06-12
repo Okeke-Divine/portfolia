256778f5f6f79139d53045f161de39dee0368784
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:28:47 +0100

    Updated

---
+++ src/components/i/portfolio/PortfolioAbout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PortfolioAbout = () => {
+  return (
+    <>
+        <section className="app-portfolio-padding bg-base-100" id="about">
+
+        </section>
+    </>
+    )
+}
+
+export default PortfolioAbout
