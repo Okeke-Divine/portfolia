f3309c18fef009098680329b9289352ad1167afb
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 16:25:28 +0100

    Updated

--- src/components/i/portfolio/PortfolioProjects.jsx
+++ src/components/i/portfolio/PortfolioProjects.jsx
@@ -18,7 +18,7 @@ const PortfolioProjects = () => {
           <h1 className="text-center text-4xl">My Creative Works</h1>
         </div>
         <div>
-          <div className="grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-4">
+          <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 mt-2">
             {projects.map((project, index) => (
               <>
                 <div className="card shadow-xl">
