344b6d8f6c2a986ca59a3d91ef6e709d1b11d1ee
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 16:08:57 +0100

    Updated

---
+++ src/components/i/portfolio/PortfolioFooter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PortfolioFooter = () => {
+  return (
+    <>
+      <div className="footer p-5 bg-base-200">Footer</div>
+    </>
+  );
+};
+
+export default PortfolioFooter;
