10d2cb610c1c14072a3e8b03969acfc8b2131bc3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 16:46:41 +0100

    Updated

--- src/app/(layout-1)/portfolio/builder/page.jsx
+++ src/app/(layout-1)/portfolio/builder/page.jsx
@@ -1,15 +1,19 @@
 import { getCurrentSession } from "@/utils/session";
-import Link from "next/link";
 
 export const metadata = {
-  title: "Portfolio Builder"
-}
+  title: "Portfolio Builder",
+};
 
 const PortfolioBuilder = async () => {
   const session = await getCurrentSession();
   return (
     <>
-    Preveiw
+      <div className="mockup-browser border bg-base-300">
+        <div className="mockup-browser-toolbar">
+          <div className="input text-center">{process.env.NEXTAUTH_URL}{session.user.username}</div>
+        </div>
+        <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+      </div>
     </>
   );
 };
