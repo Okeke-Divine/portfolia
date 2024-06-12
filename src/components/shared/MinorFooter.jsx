022b50edbf4f65676dedb37f36b242c4a26083bc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 12:22:31 +0100

    Updated

--- src/components/shared/MinorFooter.jsx
+++ src/components/shared/MinorFooter.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import Link from "next/link";
 
 const MinorFooter = () => {
   return (
     <>
-        <div className="footer p-5 md:p-10 bg-black text-center">
-            Test
+      <div className="footer p-5 md:p-10 bg-base-300">
+        <div className="w-full">
+          <div className="w-full text-center text-[18px] font-semibold">
+            A product of{" "}
+            <Link href={"#"} className="app-text-primary font-bold">
+              Skiyen
+            </Link>
+          </div>
+          <div className="w-full text-center  text-[18px] font-semibold">
+            Built by{" "}
+            <Link
+              href={""}
+              target="_blank"
+              className="app-text-primary font-bold"
+            >
+              Dev Divine
+            </Link>
+          </div>
         </div>
+      </div>
     </>
-    )
-}
+  );
+};
 
-export default MinorFooter
+export default MinorFooter;
