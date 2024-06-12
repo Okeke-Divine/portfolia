15fc8d8048fa4ff921dcc91f8db41b7d420cfd79
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 01:13:23 +0100

    Updated

--- src/components/shared/ApplicationNavbar.jsx
+++ src/components/shared/ApplicationNavbar.jsx
@@ -4,7 +4,7 @@ import config from "@/data/config.json"
 export default function ApplicationNavbar() {
   return (
     <>
-      <div className="navbar bg-base-200">
+      <div className="navbar bg-base-200 px-5 md:px-10">
         <div className="navbar-start">
         </div>
         <div className="navbar-center font-bold uppercase">
