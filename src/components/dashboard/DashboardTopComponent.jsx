d5a02492eb79f4a2ba6043d11c788f0b3bc1ec2e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 12:10:41 +0100

    Updated

--- src/components/dashboard/DashboardTopComponent.jsx
+++ src/components/dashboard/DashboardTopComponent.jsx
@@ -16,9 +16,7 @@ const DashboardTopComponent = async () => {
         {/* portfolio analytics */}
         <div className="divider"></div>
         <div>
-          <h3 className="flex gap-2 items-center">
-          <i className="fi fi-ts-chart-arrow-grow text-[18px]"></i>
-          Portfolio</h3>
+          <h3>Portfolio</h3>
           <div>
             <div className="join stats-vertical lg:stats-horizontal w-full stats shadow-md duration-100 hover:shadow-lg">
               <div className="join-item stat">
@@ -26,28 +24,44 @@ const DashboardTopComponent = async () => {
                 <div className="stat-title">Page Views</div>
                 <div className="stat-value">0k</div>
                 <div className="stat-desc">0% more than last month</div>
-                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+                <div className="stat-desc mt-2">
+                  <span className="badge app-bg-primary border-none text-white">
+                    coming soon
+                  </span>
+                </div>
               </div>
               <div className="join-item stat">
                 <div className="stat-figure">53.5k</div>
                 <div className="stat-title">Page Clicks</div>
                 <div className="stat-value">0k</div>
                 <div className="stat-desc">0% more than last month</div>
-                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+                <div className="stat-desc mt-2">
+                  <span className="badge app-bg-primary border-none text-white">
+                    coming soon
+                  </span>
+                </div>
               </div>
               <div className="join-item stat">
                 <div className="stat-figure">53.5k</div>
                 <div className="stat-title">Coversion</div>
                 <div className="stat-value">0%</div>
                 <div className="stat-desc">0% more than last month</div>
-                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+                <div className="stat-desc mt-2">
+                  <span className="badge app-bg-primary border-none text-white">
+                    coming soon
+                  </span>
+                </div>
               </div>
               <div className="join-item stat">
                 <div className="stat-figure">53.5k</div>
                 <div className="stat-title">Bounce Rate</div>
                 <div className="stat-value">0%</div>
                 <div className="stat-desc">0% more than last month</div>
-                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+                <div className="stat-desc mt-2">
+                  <span className="badge app-bg-primary border-none text-white">
+                    coming soon
+                  </span>
+                </div>
               </div>
             </div>
           </div>
@@ -63,21 +77,35 @@ const DashboardTopComponent = async () => {
                 <div className="stat-title">Views</div>
                 <div className="stat-value">0k</div>
                 <div className="stat-desc">0% more than last month</div>
-                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+                <div className="stat-desc mt-2">
+                  <span className="badge app-bg-primary border-none text-white">
+                    coming soon
+                  </span>
+                </div>
               </div>
               <div className="join-item stat">
                 <div className="stat-figure">53.5k</div>
                 <div className="stat-title">Clicks</div>
                 <div className="stat-value">0k</div>
                 <div className="stat-desc">0% more than last month</div>
-                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+                <div className="stat-desc mt-2">
+                  <span className="badge app-bg-primary border-none text-white">
+                    coming soon
+                  </span>
+                </div>
               </div>
               <div className="join-item stat">
-                <div className="stat-figure">53.5k</div>
+                <div className="stat-figure">
+                <i className="fi fi-tr-cloud-download-alt text-xl"></i>
+                </div>
                 <div className="stat-title">Downloads</div>
                 <div className="stat-value">0k</div>
                 <div className="stat-desc">0% more than last month</div>
-                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+                <div className="stat-desc mt-2">
+                  <span className="badge app-bg-primary border-none text-white">
+                    coming soon
+                  </span>
+                </div>
               </div>
             </div>
           </div>
