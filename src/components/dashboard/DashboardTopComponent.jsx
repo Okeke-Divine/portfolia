7cd2ead2dab9c946d38fdd1be35af742715a6cf7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 11:54:57 +0100

    Updated

--- src/components/dashboard/DashboardTopComponent.jsx
+++ src/components/dashboard/DashboardTopComponent.jsx
@@ -1,25 +1,25 @@
-import { getCurrentSession } from "@/utils/session"
+import { getCurrentSession } from "@/utils/session";
 
 const DashboardTopComponent = async () => {
+  const session = await getCurrentSession();
 
-    const session = await getCurrentSession();
-    for(let i = 0; i< 1000; i++){
-        console.log(i);
-    }
-
-    return (
+  return (
     <>
-         <div>
-          <div>
-            <h1 className="flex items-center gap-2">
-              <i className="fi fi-tr-house-chimney titleIcon"></i> Welcome,
-              {session.user.name}
-            </h1>
-            <p>No code Portfolio & Resume builder.</p>
-          </div>
+      <div>
+        <div>
+          <h1 className="flex items-center gap-2">
+            <i className="fi fi-tr-house-chimney titleIcon"></i> Welcome,{" "}
+            <span className="capitalize">{session.user.name}</span>
+          </h1>
+          <p>No code Portfolio & Resume builder.</p>
         </div>
+        <div className="divider"></div>
+        <div>
+            
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DashboardTopComponent
+export default DashboardTopComponent;
