57d1b5c568bcfcc28fe01e50c8705e449b5b1e84
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 12:05:34 +0100

    Updated

--- src/components/dashboard/DashboardTopComponent.jsx
+++ src/components/dashboard/DashboardTopComponent.jsx
@@ -17,7 +17,7 @@ const DashboardTopComponent = async () => {
         <div>
           <h3>Portfolio</h3>
           <div>
-            <div className="join stats-vertical lg:stats-horizontal grid grid-cols-1 lg:grid-cols-3 stats shadow-md duration-100 hover:shadow-lg">
+            <div className="join stats-vertical lg:stats-horizontal w-full stats shadow-md duration-100 hover:shadow-lg">
               <div className="join-item stat">
                 <div className="stat-figure">53.5k</div>
                 <div className="stat-title">Page Views</div>
@@ -36,6 +36,13 @@ const DashboardTopComponent = async () => {
                 <div className="stat-figure">53.5k</div>
                 <div className="stat-title">Page Views</div>
                 <div className="stat-value">0k</div>
+                <div className="stat-desc">0% more than last month</div>
+                <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
+              </div>
+              <div className="join-item stat">
+                <div className="stat-figure">53.5k</div>
+                <div className="stat-title">Page Conversion</div>
+                <div className="stat-value">0%</div>
                 <div className="stat-desc">0% more than last month</div>
                 <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
               </div>
