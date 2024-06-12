1822515e63dcfd61dc1cf4e66402552d1719c8f4
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 12:09:50 +0100

    Updated

--- src/components/dashboard/DashboardTopComponent.jsx
+++ src/components/dashboard/DashboardTopComponent.jsx
@@ -16,7 +16,9 @@ const DashboardTopComponent = async () => {
         {/* portfolio analytics */}
         <div className="divider"></div>
         <div>
-          <h3>Portfolio</h3>
+          <h3 className="flex gap-2 items-center">
+          <i className="fi fi-ts-chart-arrow-grow text-[18px]"></i>
+          Portfolio</h3>
           <div>
             <div className="join stats-vertical lg:stats-horizontal w-full stats shadow-md duration-100 hover:shadow-lg">
               <div className="join-item stat">
@@ -72,7 +74,7 @@ const DashboardTopComponent = async () => {
               </div>
               <div className="join-item stat">
                 <div className="stat-figure">53.5k</div>
-                <div className="stat-title">Download</div>
+                <div className="stat-title">Downloads</div>
                 <div className="stat-value">0k</div>
                 <div className="stat-desc">0% more than last month</div>
                 <div className="stat-desc mt-2"><span className="badge app-bg-primary border-none text-white">coming soon</span></div>
