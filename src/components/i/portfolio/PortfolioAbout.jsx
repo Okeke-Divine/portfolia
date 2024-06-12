52fd11c5608fb0340234a5db1fa7254f8a730b1e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:30:35 +0100

    Updated

--- src/components/i/portfolio/PortfolioAbout.jsx
+++ src/components/i/portfolio/PortfolioAbout.jsx
@@ -4,7 +4,9 @@ const PortfolioAbout = () => {
   return (
     <>
         <section className="app-portfolio-padding bg-base-100" id="about">
-            .
+            <div>
+                <div className="app-text-light-2 uppercase text-sm font-semibold">Nice to meet you.</div>
+            </div>
         </section>
     </>
     )
