fc355b8f1c7ccda9cb1641e4056d33bcabf06273
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 17:19:22 +0100

    Updated

--- src/components/portfolio/builder/PortfolioBuilderPreview.jsx
+++ src/components/portfolio/builder/PortfolioBuilderPreview.jsx
@@ -1,43 +1,68 @@
 "use client";
-import {useState} from "react"
+import { useState } from "react";
 
-const PortfolioBuilderPreview = ({username}) => {
-
-  const [preview,setPreview] = useState("desktop");
+const PortfolioBuilderPreview = ({ username }) => {
+  const [preview, setPreview] = useState("desktop");
 
   return (
     <>
-    <button className={`btn btn-ghost hover:app-bg-primary-dark hover:text-white ${preview == "desktop" ? "app-bg-primary-dark text-white": ""}`} onClick={() => setPreview('desktop')}>Desktop</button>
-    <button className={`btn btn-ghost hover:app-bg-primary-dark hover:text-white ${preview == "mobile" ? "app-bg-primary-dark text-white": ""}`} onClick={() => setPreview('mobile')}>Mobile</button>
+      <button
+        className={`btn btn-ghost hover:app-bg-primary-dark hover:text-white ${
+          preview == "desktop" ? "app-bg-primary-dark text-white" : ""
+        }`}
+        onClick={() => setPreview("desktop")}
+      >
+        Desktop
+      </button>
+      <button
+        className={`btn btn-ghost hover:app-bg-primary-dark hover:text-white ${
+          preview == "mobile" ? "app-bg-primary-dark text-white" : ""
+        }`}
+        onClick={() => setPreview("mobile")}
+      >
+        Mobile
+      </button>
 
-    <div className="divider"></div>
+      <div className="divider"></div>
 
-    {preview == "desktop" ? (<>
-      <div className="mockup-browser border bg-base-300">
-        <div className="mockup-browser-toolbar">
-          <div className="input text-center">
-            {process.env.NEXT_PUBLIC_BASE_URL}i/{username}
+      {preview == "desktop" ? (
+        <>
+          <div className="mockup-browser border bg-base-300">
+            <div className="mockup-browser-toolbar">
+              <div className="input text-center">
+                {process.env.NEXT_PUBLIC_BASE_URL}i/{username}
+              </div>
+            </div>
+            <div className="">
+              <iframe
+                className="w-full min-h-[50vh]"
+                src={"/i/" + username}
+              ></iframe>
+            </div>
           </div>
-        </div>
-        <div className="">
-          <iframe
-            className="w-full min-h-[50vh]"
-            src={"/i/" + username}
-          ></iframe>
-        </div>
-      </div></>) : (<></>)}
-      {preview == "mobile" ? (<>
-        <div className="mockup-phone">
-        <div className="camera"></div>
-        <div className="display">
-          <iframe
-            className="w-full min-h-[80vh]"
-            src={"/i/" + username}
-          ></iframe>
-        </div>
-      </div></>) : (<></>)}
+        </>
+      ) : (
+        <></>
+      )}
+      {preview == "mobile" ? (
+        <>
+          <div className="flex justify-center">
+            <div className="mockup-phone">
+              <div className="camera"></div>
+              <div className="display">
+                <iframe
+                  className="w-full min-h-[80vh]"
+                  src={"/i/" + username}
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default PortfolioBuilderPreview
+export default PortfolioBuilderPreview;
