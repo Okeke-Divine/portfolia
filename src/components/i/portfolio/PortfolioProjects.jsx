b3b227ea44d7266a6501d4b3991ef43a78f346a4
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 16:13:45 +0100

    Updated

--- src/components/i/portfolio/PortfolioProjects.jsx
+++ src/components/i/portfolio/PortfolioProjects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const PortfolioProjects = () => {
+  const projects = [{}];
   return (
     <>
       <section className="app-portfolio-padding" id="projects">
@@ -9,6 +10,35 @@ const PortfolioProjects = () => {
             Portfolio
           </div>
           <h1 className="text-center text-4xl">My Creative Works</h1>
+        </div>
+        <div>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+            {projects.map((project, index) => (
+              <>
+                <div className="card shadow-xl">
+                  <figure>
+                    <img
+                      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                      alt="Shoes"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">
+                      Shoes! <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                      <div className="badge badge-outline">Fashion</div>
+                      <div className="badge badge-outline">Products</div>
+                    </div>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ))}
+          </div>
         </div>
       </section>
     </>
