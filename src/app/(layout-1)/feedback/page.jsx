571050f8034844c3f00211e5ce2409f82065e8f1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 10:44:25 +0100

    Updated

--- src/app/(layout-1)/feedback/page.jsx
+++ src/app/(layout-1)/feedback/page.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const FeedBack = () => {
   return (
     <>
-    <div>
       <div>
-        <h1>Send Feedback</h1>
-        <p>Shape your experience</p>
+        <div>
+          <h1 className="">
+            <i className="fi fi-tr-comment-alt-middle flaticon-offset text-[20px]"></i>Send Feedback
+          </h1>
+          <p>Shape your experience</p>
+        </div>
       </div>
-    </div>
     </>
-    )
-}
+  );
+};
 
-export default FeedBack
+export default FeedBack;
