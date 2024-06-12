62ee2fe032cccaacb06b8cf15d0187a351537e43
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 14:40:44 +0100

    Updated

--- src/components/i/portfolio/PortfolioNavbar.jsx
+++ src/components/i/portfolio/PortfolioNavbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { defaultImgUrl } from "@/constants/shared/constant";
+import Link from "next/link";
 
 const PortfolioNavbar = () => {
   return (
-    <div>PortfolioNavbar</div>
-  )
-}
+    <>
+      <div className="navbar">
+        <div className="navbar-start">
+          <div className="avatar cursor-pointer">
+            <div className="w-10 rounded-full ring ring-green-300">
+              <img src={defaultImgUrl} alt="User Profile Picture" />
+            </div>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <Link href="" className="">
+            About
+          </Link>
+        </div>
+        <div className="navbar-end"></div>
+      </div>
+    </>
+  );
+};
 
-export default PortfolioNavbar
+export default PortfolioNavbar;
