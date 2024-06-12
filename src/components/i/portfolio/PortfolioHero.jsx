bd614444cb62775a706ef0026190de4cfa2806a0
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:08:39 +0100

    Updated

--- src/components/i/portfolio/PortfolioHero.jsx
+++ src/components/i/portfolio/PortfolioHero.jsx
@@ -6,7 +6,13 @@ const PortfolioHero = () => {
       <div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 py-5 md:py-10 px-10 md:px-36">
           <div>
-            <h1 className="max-w-[500px] text-5xl">Hey, I'm a full stack web developer</h1>
+            <h1 className="max-w-[500px] text-5xl">
+              Hey, I'm a full stack web developer
+            </h1>
+            <p className="app-text-light">
+              Hi, i'm Jason a freelancer web designer from Nigeria. I help
+              brands turn their ideas into high quality products
+            </p>
           </div>
           <div></div>
         </div>
