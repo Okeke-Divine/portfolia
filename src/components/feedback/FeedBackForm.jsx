94a25ef48d7c4cdf28f42ec6906bc6648cbeb761
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 10:59:39 +0100

    Updated

--- src/components/feedback/FeedBackForm.jsx
+++ src/components/feedback/FeedBackForm.jsx
@@ -1,24 +1,53 @@
 "use client";
+import { useState, useRef } from "react";
 
 const FeedBackForm = () => {
+  const messageRef = useRef(null);
+  const ratingRef = useRef(null);
+
+  // sendFeedback
+  function sendFeedback() {}
+
   return (
     <>
-      <form className="form-control">
+      <form className="form-control" onSubmit={sendFeedback}>
         <h3 className="font-bold text-xl">
           We read them all! How can we improve your experience?
         </h3>
         {/* rating */}
         <div className="mt-3">
           <div className="rating">
-            <input type="radio" name="rating-1" className="mask mask-star" />
             <input
               type="radio"
               name="rating-1"
               className="mask mask-star"
+              ref={ratingRef}
             />
-            <input type="radio" name="rating-1" className="mask mask-star" />
-            <input type="radio" name="rating-1" className="mask mask-star" defaultChecked={true} />
-            <input type="radio" name="rating-1" className="mask mask-star" />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              ref={ratingRef}
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              ref={ratingRef}
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              ref={ratingRef}
+              defaultChecked={true}
+            />
+            <input
+              type="radio"
+              name="rating-1"
+              className="mask mask-star"
+              ref={ratingRef}
+            />
           </div>
         </div>
         {/* message */}
@@ -30,13 +59,17 @@ const FeedBackForm = () => {
         <div>
           <textarea
             rows={7}
+            ref={messageRef}
             className="textarea textarea-bordered w-full"
             placeholder="Write your feedback..."
+            required
           ></textarea>
         </div>
         {/* button */}
         <div className="mt-2">
-          <button className="app-primary-button">Send Feedback</button>
+          <button type="submit" className="app-primary-button">
+            Send Feedback
+          </button>
         </div>
       </form>
     </>
