d0e3734681c7b52f972520b5a2605216ae0f593b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 10:49:38 +0100

    Updated

--- src/components/feedback/FeedBackForm.jsx
+++ src/components/feedback/FeedBackForm.jsx
@@ -4,13 +4,13 @@ const FeedBackForm = () => {
   return (
     <>
     <form className="form-control">
-        <h3>We read them all! How can we improve your experience?</h3>
+        <h3 className="font-bold text-xl">We read them all! How can we improve your experience?</h3>
         {/* message */}
-        <div>
-            <label htmlFor="msg">Message</label>
+        <div className="mb-2">
+            <label htmlFor="msg" className="font-semibold">Message</label>
         </div>
         <div>
-            <textarea cols="30" rows="10" className="textarea" placeholder="Write your feedback..."></textarea>
+            <textarea rows={5} className="textarea textarea-bordered w-full" placeholder="Write your feedback..."></textarea>
         </div>
     </form>
     </>
