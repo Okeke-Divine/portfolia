f7b4836be12053cf387e0df77c3b565887aa9c81
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 16:31:03 +0100

    Updated

--- src/app/(layout-portfolio)/i/[username]/page.jsx
+++ src/app/(layout-portfolio)/i/[username]/page.jsx
@@ -1,3 +1,4 @@
+import prisma from "@/app/db";
 import PortfolioAbout from "@/components/i/portfolio/PortfolioAbout";
 import PortfolioFooter from "@/components/i/portfolio/PortfolioFooter";
 // import PortfolioExpertise from "@/components/i/portfolio/PortfolioExpertise";
@@ -5,13 +6,31 @@ import PortfolioHero from "@/components/i/portfolio/PortfolioHero";
 import PortfolioNavbar from "@/components/i/portfolio/PortfolioNavbar";
 import PortfolioProjects from "@/components/i/portfolio/PortfolioProjects";
 
-export function generateMetadata({ params }) {
+export async function generateMetadata({ params }) {
+  const username = params.username;
+  const user = await prisma.user.count({where: {username}})
+  if(!user){
+    return {
+      title: "404 | Not Found"
+    }
+  }
   return {
     title: params.username,
   };
 }
 
-export default function PortFolio({ params }) {
+export default async function PortFolio({ params }) {
+  const username = params.username;
+  const user = await prisma.user.count({where: {username}})
+  
+  if(!user){
+    return (
+      <>
+        404
+      </>
+    )
+  }
+
   return (
     <>
       <PortfolioNavbar />
