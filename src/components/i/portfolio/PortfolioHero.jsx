fdda588508d6f0b4da7df25e447d4154891bfcd1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:21:14 +0100

    Updated

--- src/components/i/portfolio/PortfolioHero.jsx
+++ src/components/i/portfolio/PortfolioHero.jsx
@@ -5,29 +5,34 @@ const PortfolioHero = () => {
     <>
       <div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 py-5 md:py-10 px-10 md:px-36 place-content-center min-h-[60vh]">
+          {/* left */}
           <div className="flex items-center justify-end">
-            <div className="w-fit h-fit"><h1 className="max-w-[500px] text-6xl">Hey, I'm John</h1>
-            <p className="app-text-light-2 py-2 max-w-[400px] font-semibold underline text-2xl">
-              Full Stack Web Developer @ Figma
-            </p>
-            <p className="app-text-light-2 py-2 max-w-[400px]">
-              Hi, i'm Jason a freelancer web designer from Nigeria. I help
-              brands turn their ideas into high quality products.
-            </p>
-            <div className="pt-2">
-              <button className="app-primary-button w-fit">
-                Message
-                <i className="fi fi-rr-paper-plane-top flaticon-offset"></i>
-              </button>
-            </div></div>
+            <div className="w-full h-fit">
+              <h1 className="max-w-[500px] text-6xl">Hey, I'm John</h1>
+              <p className="app-text-light-2 py-2 max-w-[400px] font-semibold underline text-2xl">
+                Full Stack Web Developer @ Figma
+              </p>
+              <p className="app-text-light-2 py-2 max-w-[400px]">
+                Hi, i'm Jason a freelancer web designer from Nigeria. I help
+                brands turn their ideas into high quality products.
+              </p>
+              <div className="pt-2">
+                <button className="app-primary-button w-fit">
+                  Message
+                  <i className="fi fi-rr-paper-plane-top flaticon-offset"></i>
+                </button>
+              </div>
+            </div>
           </div>
           {/* right content */}
           <div>
-            <div className="avatar cursor-pointer">
-              <div className="rounded-full ring ring-green-300 w-96">
+           <div className="flex justify-center md:justify-end items-center">
+           <div className="avatar cursor-pointer">
+              <div className="rounded-full ring ring-green-300 w-52 lg:w-96">
                 <img src={defaultImgUrl} alt="User Profile Picture" />
               </div>
             </div>
+           </div>
           </div>
         </div>
       </div>
