1b60a935c438c54c4f364aeab80ef546e179685e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 15:58:01 +0100

    Updated

---
+++ src/components/i/portfolio/PortfolioExpertise.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const PortfolioExpertise = () => {
+  return (
+    <>
+      <div className="app-portfolio-padding">
+        <div>
+          <div className="app-text-light-2 uppercase font-semibold tracking-wide text-xl mb-2">
+            My Skills
+          </div>
+          <h1 className="max-w-[80%] md:max-w-[600px] text-4xl">
+            My Expertise
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PortfolioExpertise;
