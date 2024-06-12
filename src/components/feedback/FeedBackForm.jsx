2c15cc7746656a90d45fc2951ca6ecd0b76b0927
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-12 10:51:56 +0100

    Updated

--- src/components/feedback/FeedBackForm.jsx
+++ src/components/feedback/FeedBackForm.jsx
@@ -1,20 +1,32 @@
-"use client"
+"use client";
 
 const FeedBackForm = () => {
   return (
     <>
-    <form className="form-control">
-        <h3 className="font-bold text-xl">We read them all! How can we improve your experience?</h3>
+      <form className="form-control">
+        <h3 className="font-bold text-xl">
+          We read them all! How can we improve your experience?
+        </h3>
         {/* message */}
         <div className="mb-2">
-            <label htmlFor="msg" className="font-semibold">Message</label>
+          <label htmlFor="msg" className="font-semibold">
+            Message
+          </label>
         </div>
         <div>
-            <textarea rows={5} className="textarea textarea-bordered w-full" placeholder="Write your feedback..."></textarea>
+          <textarea
+            rows={7}
+            className="textarea textarea-bordered w-full"
+            placeholder="Write your feedback..."
+          ></textarea>
         </div>
-    </form>
+        {/* button */}
+        <div className="mt-2">
+          <button className="app-primary-button">Send Feedback</button>
+        </div>
+      </form>
     </>
-    )
-}
+  );
+};
 
-export default FeedBackForm
+export default FeedBackForm;
