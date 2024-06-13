d04ae0abbfbe220807a323e522d72ced7bde948e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-13 20:58:42 +0100

    Updated

--- src/app/(layout-1)/info/layout.jsx
+++ src/app/(layout-1)/info/layout.jsx
@@ -9,7 +9,7 @@ export default function InfoLayout({ children }) {
           <h1 className="flex items-center gap-2">
             <i className="fi fi-ts-web-design titleIcon"></i> Info
           </h1>
-          <p className="app-text-light">Enter once, use anywhere</p>
+          <p className="app-text-light">Write once, use anywhere</p>
         </div>
         <div className="divider"></div>
         <div>
