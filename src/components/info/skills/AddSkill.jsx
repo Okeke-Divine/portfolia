eed72b6dde66376361109985ac5658b86f23f111
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-13 21:11:57 +0100

    Updated

--- src/components/info/skills/AddSkill.jsx
+++ src/components/info/skills/AddSkill.jsx
@@ -1,11 +1,31 @@
 "use client";
+import { useRef } from "react/cjs/react.production.min";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 const AddSkill = () => {
+  const skillRef = useRef(null);
+
+  function _addSkill() {
+    const skill = skillRef.current.value;
+    if (!skill || skill.length < 3) {
+      Swal.fire({
+        title: "Error",
+        icon: "error",
+        text: "Skill can not be less than three characters",
+      });
+      return;
+    }
+
+    //pass
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={_addSkill}>
         <div className="join flex">
           <input
+            ref={skillRef}
             type="text"
             className="join-item grow input input-borderd"
             placeholder="Skill Name"
