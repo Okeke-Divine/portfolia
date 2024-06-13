cd207abb2130a0a1da3c81c9397060ab1a2b12bc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-13 21:09:42 +0100

    Updated

--- src/app/(layout-1)/info/skills/page.jsx
+++ src/app/(layout-1)/info/skills/page.jsx
@@ -1,11 +1,17 @@
+import AddSkill from "@/components/info/skills/AddSkill"
+
 export const metadata = {
     title: "Skills | Info"
 }
 
 const InfoSkills = () => {
   return (
-    <div>InfoSkills</div>
-  )
+    <>
+      <div>
+        <AddSkill />
+      </div>
+    </>
+    )
 }
 
 export default InfoSkills
