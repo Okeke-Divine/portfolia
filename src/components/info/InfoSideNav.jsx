dfdd661257971907ada8e02bb99008b025e6f0d5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-13 20:57:57 +0100

    Updated

--- src/components/info/InfoSideNav.jsx
+++ src/components/info/InfoSideNav.jsx
@@ -1,6 +1,6 @@
 "use client";
-
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
   { name: "Basic Info", url: "/info" },
@@ -9,13 +9,17 @@ const links = [
 ];
 
 export default function InfoSideNav() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link, index) => (
         <Link
           key={index}
           href={link.url}
-          className={`btn join-item font-semibold hover:app-bg-primary-dark app-bg-primary text-white`}
+          className={`btn join-item font-semibold hover:app-bg-primary-dark hover:text-white duration-300 ${
+            pathname == link.url ? "app-bg-primary text-white" : "text-black"
+          }`}
         >
           {link.name}
         </Link>
