9e79b1e9b76d2d5783ff7f1bd5e81eabc5cf206c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-13 21:10:09 +0100

    Updated

--- src/components/info/skills/AddSkill.jsx
+++ src/components/info/skills/AddSkill.jsx
@@ -4,9 +4,15 @@ const AddSkill = () => {
   return (
     <>
       <form>
-        <div className="join">
-          <input type="text" className="join-item grow input input-borderd" />
-          <button className="join-item btn app-bg-primary text-white duration-300 hover:app-bg-primary-dark">Add</button>
+        <div className="join flex">
+          <input
+            type="text"
+            className="join-item grow input input-borderd"
+            placeholder="Skill Name"
+          />
+          <button className="join-item btn app-bg-primary text-white duration-300 hover:app-bg-primary-dark">
+            Add
+          </button>
         </div>
       </form>
     </>
