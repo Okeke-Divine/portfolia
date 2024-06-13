688cbd14dfc306cce5594ec71beaed31df30b998
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-13 21:39:57 +0100

    Updated

--- src/components/info/skills/ListSkills.jsx
+++ src/components/info/skills/ListSkills.jsx
@@ -5,20 +5,17 @@ const ListSkills = async () => {
   const userId = await getUserId();
   const skills = await prisma.userSkills.findMany({
     where: { userId },
-    select: { skill_name: true },
   });
 
   console.log(skills);
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 lg:grid-cols-6 xl:grid-cols-8">
+      <div className="grid grid-cols-1 gap-2">
         {skills.map((skill, index) => (
           <>
-            <div
-              key={index}
-              className="rounded-lg shadow-sm duration-300 hover:shadow-lg bg-white text-center p-5"
-            >
-              <div className="capitalize font-bold">{skill.skill_name}</div>
+            <div className="flex flex-wrap gap">
+              <div className="grow">{skill.skill_name}</div>
+              <div>[x]</div>
             </div>
           </>
         ))}
