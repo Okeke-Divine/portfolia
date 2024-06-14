eaab257a738a92938264c8b28d8ad8ae274c660a
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 00:38:08 +0100

    Updated

--- src/components/i/portfolio/PortfolioHero.jsx
+++ src/components/i/portfolio/PortfolioHero.jsx
@@ -24,7 +24,7 @@ const PortfolioHero = ({ user }) => {
           <div className="flex items-center justify-end">
             <div className="w-full h-fit">
               <h1 className="max-w-[500px] text-6xl">
-                Hey, I'm {user?.userDetails?.fullname}
+                {user?.userDetails?.heroTitle}
               </h1>
               <p className="app-text-light-2 py-2 max-w-[400px] font-semibold underline text-2xl">
                 {user?.userDetails?.profession}
