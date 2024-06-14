618388cad055a2912b3d981fd3ef91c1682a829f
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-14 19:27:36 +0100

    Updated

--- src/components/info/InfoSideNav.jsx
+++ src/components/info/InfoSideNav.jsx
@@ -1,16 +1,17 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import ComingSoonComponent from "../shared/ComingSoonComponent";
 
 const links = [
-  { name: "Basic Info", url: "/info",shipped:true },
-  { name: "Skills", url: "/info/skills",shipped:true },
-  { name: "Projects", url: "/info/projects",shipped:true },
-  { name: "Experience", url: "/info/experience",shipped:false },
-  { name: "Education", url: "/info/education",shipped:false },
-  { name: "Language", url: "/info/language",shipped:false },
-  { name: "Certificates", url: "/info/certificates",shipped:false },
-  { name: "Awards", url: "/info/awards",shipped:false },
+  { name: "Basic Info", url: "/info", shipped: true },
+  { name: "Skills", url: "/info/skills", shipped: true },
+  { name: "Projects", url: "/info/projects", shipped: true },
+  { name: "Experience", url: "/info/experience", shipped: false },
+  { name: "Education", url: "/info/education", shipped: false },
+  { name: "Language", url: "/info/language", shipped: false },
+  { name: "Certificates", url: "/info/certificates", shipped: false },
+  { name: "Awards", url: "/info/awards", shipped: false },
 ];
 
 export default function InfoSideNav() {
@@ -21,12 +22,19 @@ export default function InfoSideNav() {
       {links.map((link, index) => (
         <Link
           key={index}
-          href={link.url}
+          href={link.shipped ? link.url : "#"}
           className={`btn join-item font-semibold hover:app-bg-primary-dark hover:text-white duration-300 ${
             pathname == link.url ? "app-bg-primary text-white" : "text-black"
           }`}
         >
           {link.name}
+          {!link.shipped ? (
+            <>
+              <ComingSoonComponent />
+            </>
+          ) : (
+            ""
+          )}
         </Link>
       ))}
     </>
