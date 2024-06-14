f2c910160dfcb5d1f3e348999e395e8033400611
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 00:13:30 +0100

    Updated

--- src/app/api/info/basic-info/route.js
+++ src/app/api/info/basic-info/route.js
@@ -3,6 +3,7 @@ import { internalServerError, resourceUpdated } from "@/utils/prebuiltApiRespons
 export const POST = async (req) => {
     try {
         const data = await req.json();
+        const { fullname, heroTitle, profession, bio, about } = data;
         console.log(data);
         return resourceUpdated({})
     } catch (e) {
