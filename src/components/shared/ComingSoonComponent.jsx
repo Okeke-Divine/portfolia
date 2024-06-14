7465d4e91525f924f8a95c9cb34a7b790d9bff32
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-14 19:24:20 +0100

    Updated

---
+++ src/components/shared/ComingSoonComponent.jsx
@@ -0,0 +1,9 @@
+export default function ComingSoonComponent() {
+  return (
+    <>
+      <span className="badge app-bg-primary text-white border-none badge-sm">
+        <div className="loading loading-dots loading-xs"></div>
+      </span>
+    </>
+  );
+}
