e11c0d1e8be47522e426be30124c4a9b081a6b87
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-14 23:56:55 +0100

    Updated

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,18 +1,21 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth-options";
+import { redirect } from "next/navigation";
 
 export async function getCurrentSession() {
   const session = await getServerSession(authOptions)
-  if(!session){
+  if (!session) {
     return null
   }
   return session;
 }
 
-export async function getUserId(){
+export async function getUserId() {
+  "use server"
   const session = await getCurrentSession();
-  const userId = session.user.id;
-  if(!userId){
+  const userId = session?.user?.id;
+  if (!userId) {
+    redirect("/signin")
     return null;
   }
   return userId;
