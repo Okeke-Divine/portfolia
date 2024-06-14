0525510d81c39c2d6d5bad2b1ac77d289457faf2
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-14 20:02:27 +0100

    Updated

--- src/components/info/skills/ListSkills.jsx
+++ src/components/info/skills/ListSkills.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import Swal from "sweetalert2";
+import { app_color_primary } from "@/constants/shared/color";
 
 const ListSkills = async () => {
   const [skills, setSkills] = useState([]);
@@ -13,7 +14,11 @@ const ListSkills = async () => {
       title: "Warning",
       icon: "warning",
       text: "Are you sure you want to remove this skill? NOTE: This cannot be reversed.",
-    })
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete!",
+      confirmButtonColor: app_color_primary,
+      cancelButtonText: "Cancel",
+    });
   }
 
   useEffect(function () {
@@ -27,6 +32,7 @@ const ListSkills = async () => {
       <div className="grid grid-cols-1 gap-2">
         {skills.map((skill, index) => (
           <div
+            id={"skill" + skill_name}
             className={`flex flex-wrap items-center gap p-2 ${
               index % 2 === 0 ? "bg-white" : "bg-gray-200"
             }`}
@@ -34,7 +40,10 @@ const ListSkills = async () => {
           >
             <div className="grow font-semibold">{skill.skill_name}</div>
             <div>
-              <button className="btn btn-sm" onClick={() => deleteSkill(skill.skill_name)}>
+              <button
+                className="btn btn-sm"
+                onClick={() => deleteSkill(skill.skill_name)}
+              >
                 <i className="fi fi-rr-trash flaticon-offset"></i>
               </button>
             </div>
