83fe06f65f56769aa20aa414dcb924cd31de0289
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 15:05:35 +0100

    Updated

--- src/components/shared/ApplicationNavbar.jsx
+++ src/components/shared/ApplicationNavbar.jsx
@@ -1,21 +1,23 @@
 import { defaultImgUrl } from "@/constants/shared/constant";
-import config from "@/data/config.json"
+import config from "@/data/config.json";
+import Link from "next/link";
 
 export default function ApplicationNavbar() {
   return (
     <>
       <div className="navbar bg-base-200 px-5 md:px-10">
-        <div className="navbar-start">
-        </div>
+        <div className="navbar-start"></div>
         <div className="navbar-center font-bold uppercase">
-            {config.app_name}
+          {config.app_name}
         </div>
         <div className="navbar-end">
+          <Link href="/settings/profile/picture/change">
             <div className="avatar cursor-pointer">
-                <div className="w-10 rounded-full ring ring-green-300">
-                    <img src={defaultImgUrl} alt="User Profile Picture" />
-                </div>
+              <div className="w-10 rounded-full ring ring-green-300">
+                <img src={defaultImgUrl} alt="User Profile Picture" />
+              </div>
             </div>
+          </Link>
         </div>
       </div>
     </>
