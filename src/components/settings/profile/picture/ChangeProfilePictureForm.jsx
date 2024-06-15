700a80d2535f9e9d4079c0ffaaffc73f59c5b80a
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 15:28:11 +0100

    Updated

--- src/components/settings/profile/picture/ChangeProfilePictureForm.jsx
+++ src/components/settings/profile/picture/ChangeProfilePictureForm.jsx
@@ -1,6 +1,11 @@
 "use client";
+import { useRef, useState } from "react";
+import Swal from "sweetalert2";
 
 const ChangeProfilePictureForm = () => {
+  const [loading, setLoading] = useStae(true);
+  const profilePictureRef = useRef(null);
+
   return (
     <>
       <form>
@@ -8,6 +13,22 @@ const ChangeProfilePictureForm = () => {
           <label className="font-bold text-md">Select a picture</label>
         </div>
         <input type="file" accept="image/*" className="file-input" />
+        {/* submit button */}
+        <div>
+          <button
+            className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <span className="loading loading-dots loading-xs"></span>
+              </>
+            ) : (
+              "Upload"
+            )}
+          </button>
+        </div>
       </form>
     </>
   );
