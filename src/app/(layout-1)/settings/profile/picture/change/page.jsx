d26543eda430077c1adda5a588e3f5ceacc254fe
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 14:34:20 +0100

    Updated

--- src/app/(layout-1)/settings/profile/picture/change/page.jsx
+++ src/app/(layout-1)/settings/profile/picture/change/page.jsx
@@ -1,11 +1,9 @@
-import React from "react";
+export const metadata = {
+  title: "Change Profile Picture | Info",
+};
 
 const SettingsProfilePicture = () => {
-  return (
-    <>
-      settings/profile/picture/change
-    </>
-  )
+  return <>settings/profile/picture/change</>;
 };
 
 export default SettingsProfilePicture;
