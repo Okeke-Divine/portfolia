a5d9f497c2801c37d3609bd21733fd9fa1aded65
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 17:32:05 +0100

    Updated

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,19 +1,24 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "404 Not Found",
 };
 const NotFound = () => {
   return (
     <>
-    <div className="w-full h-[100vh] flex justify-center items-center">
-      <div>
-        <div className="font-bold text-3xl text-center">
-          We've lost this page
+      <div className="w-full h-[100vh] flex justify-center items-center">
+        <div>
+          <div className="font-bold text-4xl text-center">
+            We've lost this page
+          </div>
+          <p className="text-center">
+            Sorry, the page you're looking for doesn't exist or has been moved.
+          </p>
+          <Link href="/" className="btn btn-ghost">
+            Go to Home
+          </Link>
         </div>
-        <p className="text-center">
-          Sorry, the page you're looking for doesn't  exist or has been moved.
-        </p>
       </div>
-    </div>
     </>
   );
 };
