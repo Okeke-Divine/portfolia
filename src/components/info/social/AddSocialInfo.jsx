31c4ea169d2587bacfa0a54e839b2202683d61b7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 02:39:55 +0100

    Updated

--- src/components/info/social/AddSocialInfo.jsx
+++ src/components/info/social/AddSocialInfo.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+
+const social = [
+  { name: "Email" },
+  { name: "Instagram" },
+  { name: "Twitter" },
+  { name: "Facebook" },
+  { name: "Dribble" },
+  { name: "Linkedin" },
+  { name: "github" },
+  { name: "email" },
+  { name: "Phone Number" },
+  { name: "Other" },
+];
 
 const AddSocialInfo = () => {
-  return (
-    <div>AddSocialInfo</div>
-  )
-}
+  return <div>AddSocialInfo</div>;
+};
 
-export default AddSocialInfo
+export default AddSocialInfo;
