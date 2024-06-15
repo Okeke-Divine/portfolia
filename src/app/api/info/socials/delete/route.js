18693d9204df301c6c22e1b305384864704d5a95
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 13:43:51 +0100

    Updated

--- src/app/api/info/socials/delete/route.js
+++ src/app/api/info/socials/delete/route.js
@@ -1,10 +1,25 @@
-import { internalServerError, resourceDeleted } from "@/utils/prebuiltApiResponse";
+import prisma from "@/app/db";
+import { badRequest, internalServerError, resourceDeleted } from "@/utils/prebuiltApiResponse";
 
 export const POST = async (req) => {
   try {
     const data = await req.json();
     console.log(data);
-    return resourceDeleted();
+    const { social_id } = data;
+
+    if (!social_id || !parseInt(social_id)) {
+      return badRequest("social id is required/invalid social id")
+    }
+
+    const delete_social = await prisma.userSocialInfo.delete({
+      where: {
+        id: social_id
+      }
+    })
+    if (delete_social) {
+      return resourceDeleted();
+    }
+
     return internalServerError("failed to delete a social skill")
   } catch (e) {
     return internalServerError(e);
