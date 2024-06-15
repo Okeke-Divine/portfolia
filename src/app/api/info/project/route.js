b54dae2d1dacdba23bba86b86a3c576dcb21bfde
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 21:19:38 +0100

    Updated

--- src/app/api/info/project/route.js
+++ src/app/api/info/project/route.js
@@ -2,6 +2,14 @@ import { internalServerError, resourceCreated } from "@/utils/prebuiltApiRespons
 
 export const POST = async (req) => {
     try {
+        const data = await req.formData();
+        const projectTitle = data.get("projectTitle");
+        const projectTags = data.get("projectTags");
+        const projectUrl = data.get("projectUrl");
+        const projectDesc = data.get("projectDesc");
+        const projectImage = data.get("projectImage");
+        console.log(data);
+
         return resourceCreated({})
     } catch (e) {
         return internalServerError(e)
