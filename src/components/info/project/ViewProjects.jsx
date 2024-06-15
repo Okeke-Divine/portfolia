b92088122530146d46032ce84089fb3d83606247
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 22:04:19 +0100

    Updated

--- src/components/info/project/ViewProjects.jsx
+++ src/components/info/project/ViewProjects.jsx
@@ -3,6 +3,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const ViewProjects = () => {
+  const [project, setProjects] = useState([]);
+
+  useEffect(function () {
+    axios.get("/api/info/project").then((response) => {
+      setProjects(response.data.data);
+    });
+  }, []);
+
   return (
     <>
       <div>lol</div>
