78da365ad4e7c27c1557dfaf369074bc7540b4c8
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 02:57:08 +0100

    Updated

--- src/components/info/social/AddSocialInfo.jsx
+++ src/components/info/social/AddSocialInfo.jsx
@@ -1,42 +1,53 @@
 import { _ucfirst } from "@/utils/main";
-import React from "react";
+import { useRef } from "react";
 
 const socials = [
-    { type: "email", name: "email" },
-    { type: "instagram", name: "instagram" },
-    { type: "twitter", name: "twitter" },
-    { type: "facebook", name: "facebook" },
-    { type: "dribble", name: "dribble" },
-    { type: "linkedin", name: "linkedin" },
-    { type: "github", name: "github" },
-    { type: "email", name: "email" },
-    { type: "phone_number", name: "phone number" },
-    { type: "other", name: "other" },
-  ];
-  
+  { type: "email", name: "email" },
+  { type: "instagram", name: "instagram" },
+  { type: "twitter", name: "twitter" },
+  { type: "facebook", name: "facebook" },
+  { type: "dribble", name: "dribble" },
+  { type: "linkedin", name: "linkedin" },
+  { type: "github", name: "github" },
+  { type: "email", name: "email" },
+  { type: "phone_number", name: "phone number" },
+  { type: "other", name: "other" },
+];
 
 const AddSocialInfo = () => {
+  function AddSocialInfo(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <div className="">
-        <div className="block md:flex join mb-2 gap-2">
-          <div className="w-full md:w-fit join-item">
-            <select className="select input-bordered">
-              {socials.map((social, index) => (
-                <>
-                  <option value={social.value}>{_ucfirst(social.name)}</option>
-                </>
-              ))}
-            </select>
+      <form className="form-control" onSubmit={AddSocialInfo}>
+        <div className="">
+          <div className="block md:flex join mb-2 gap-2">
+            <div className="w-full md:w-fit join-item">
+              <select className="select input-bordered">
+                {socials.map((social, index) => (
+                  <>
+                    <option value={social.value}>
+                      {_ucfirst(social.name)}
+                    </option>
+                  </>
+                ))}
+              </select>
+            </div>
+            <div className="w-full join-item">
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="Enter..."
+              />
+            </div>
           </div>
-          <div className="w-full join-item">
-            <input type="text" className="input input-bordered w-full" placeholder="Enter..." />
-          </div>
+          <button className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white">
+            Add
+          </button>
         </div>
-        <button className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white">
-          Add
-        </button>
-      </div>
+      </form>
     </>
   );
 };
