06e98562dbd6980da26d928842141b857ddb104b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 14:06:15 +0100

    Uodated

--- src/components/i/portfolio/PortfolioFooter.jsx
+++ src/components/i/portfolio/PortfolioFooter.jsx
@@ -6,10 +6,10 @@ const PortfolioFooter = ({ user }) => {
     <>
       <div className="footer p-5 bg-base-300">
         <div className="w-full">
-          <div className="w-full font-bold text-xl text-center">
+          <div className="w-full font-bold text-2xl text-center">
             {user?.userDetails?.fullname}
           </div>
-          <div className="w-full font-semibold text-center">
+          <div className="w-full font-semibold text-xl text-center">
             {user?.userDetails?.profession}
           </div>
           <div className="w-full text-italics text-center">
