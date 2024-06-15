e106f7f70f3d8ed0609e6687c6f5acd8be5f3150
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 13:39:22 +0100

    Updated

--- src/components/info/social/ViewSocialInfo.jsx
+++ src/components/info/social/ViewSocialInfo.jsx
@@ -93,7 +93,7 @@ const ViewSocialInfo = () => {
 
   return (
     <>
-      {loading ? <SkillSkeleton /> : ""}
+      {loading ? (<><SkillSkeleton /></>) : ""}
       <div>
         {socials.map((social, index) => (
           <div
--- src/components/skeleton/skills/SkillSkeleton.jsx
+++ src/components/skeleton/skills/SkillSkeleton.jsx
@@ -1,13 +1,17 @@
 const SkillSkeleton = () => {
   return (
     <>
-    {[1,1,1,1,1,1,1].map((item,index) => (
+      <div className="skeleton w-full h-10 rounded-md mb-1"></div>
+      {[1, 1, 1, 1, 1, 1, 1].map((item, index) => (
         <>
-        <div key={index} className="skeleton w-full h-10 rounded-md mb-1"></div>
+          <div
+            key={index}
+            className="skeleton w-full h-10 rounded-md mb-1"
+          ></div>
         </>
-    ))}
+      ))}
     </>
-    )
-}
+  );
+};
 
-export default SkillSkeleton
+export default SkillSkeleton;
