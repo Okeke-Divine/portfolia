64caa295afd4e169a3cab5fd24b867b94b55cef1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 02:41:12 +0100

    Updated

--- src/components/info/social/AddSocialInfo.jsx
+++ src/components/info/social/AddSocialInfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const social = [
+const socials = [
   { name: "Email" },
   { name: "Instagram" },
   { name: "Twitter" },
@@ -14,7 +14,23 @@ const social = [
 ];
 
 const AddSocialInfo = () => {
-  return <div>AddSocialInfo</div>;
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <div className="join">
+          <div className="join-item">
+            <select name="" id="">
+              {socials.map((social, index) => (
+                <>
+                  <option>{social.name}</option>
+                </>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddSocialInfo;
