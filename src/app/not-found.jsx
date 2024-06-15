73e83fc6d8ee57e43b903a9b95aa4eb4227a31b5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 17:30:54 +0100

    Updated

---
+++ src/app/not-found.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "404 Not Found",
+};
+const NotFound = () => {
+  return (
+    <>
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <div>
+        <div className="font-bold text-3xl">
+          We've lost this page
+        </div>
+        <p>
+          Sorry, the page you're looking for doesn't  exist or has been moved.
+        </p>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default NotFound;
