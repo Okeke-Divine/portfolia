0bdf7b778b8fb66f5a7cc79716bf544cda3de555
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 14:05:39 +0100

    Updated

--- src/components/i/portfolio/PortfolioFooter.jsx
+++ src/components/i/portfolio/PortfolioFooter.jsx
@@ -1,14 +1,18 @@
 import config from "@/data/config.json";
 import { _ucfirst } from "@/utils/main";
 
-const PortfolioFooter = () => {
+const PortfolioFooter = ({ user }) => {
   return (
     <>
       <div className="footer p-5 bg-base-300">
         <div className="w-full">
-          <div className="w-full font-bold text-xl text-center">Divine</div>
-          <div className="w-full font-semibold text-center">Dev</div>
+          <div className="w-full font-bold text-xl text-center">
+            {user?.userDetails?.fullname}
+          </div>
           <div className="w-full font-semibold text-center">
+            {user?.userDetails?.profession}
+          </div>
+          <div className="w-full text-italics text-center">
             &copy; 2024 - {_ucfirst(config.app_name)}
           </div>
         </div>
