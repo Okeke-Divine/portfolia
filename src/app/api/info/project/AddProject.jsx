32a0ffc5353abf35352c8d9c6dbd9f751879caaa
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 20:49:52 +0100

    Updated

--- src/app/api/info/project/AddProject.jsx
+++ src/app/api/info/project/AddProject.jsx
@@ -1,6 +1,9 @@
 "use client";
+import { useState, useRef } from "react";
 
 const AddProject = () => {
+  const [loading, setLoading] = useState(false);
+
   function _addProject(e) {
     e.preventDefault();
   }
@@ -11,7 +14,24 @@ const AddProject = () => {
         <input type="checkbox" />
         <div className="collapse-title text-xl font-medium">Add Project</div>
         <div className="collapse-content">
-          <form onSubmit={_addProject} className="form-control"></form>
+          <form onSubmit={_addProject} className="form-control">
+            {/* submit button */}
+            <div>
+              <button
+                className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <span className="loading loading-dots loading-xs"></span>
+                  </>
+                ) : (
+                  "Add"
+                )}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
