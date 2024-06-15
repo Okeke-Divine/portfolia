93cb2dbef0c481fdff457e9a29b9892e8b63cef3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 22:06:13 +0100

    Updated

--- src/components/info/project/ViewProjects.jsx
+++ src/components/info/project/ViewProjects.jsx
@@ -14,7 +14,20 @@ const ViewProjects = () => {
   return (
     <>
       <div className="overflow-x-auto w-full">
-        <table></table>
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Tags</th>
+              <th>Description</th>
+              <th>Url</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
       </div>
     </>
   );
