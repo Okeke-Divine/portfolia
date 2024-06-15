7c069d50df44a17b17a95aa26ae280893466a63b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 11:30:48 +0100

    Updated

--- src/app/api/info/socials/route.js
+++ src/app/api/info/socials/route.js
@@ -1,8 +1,13 @@
-import { conflict, internalServerError, resourceCreated } from "@/utils/prebuiltApiResponse"
+import { badRequest, conflict, internalServerError, resourceCreated } from "@/utils/prebuiltApiResponse"
 
 export const POST = async (req) => {
     try {
         const data = await req.json();
+        const { socialType, socialValue } = data;
+
+        if (!socialType || !socialValue) {
+            return badRequest("social type/value is required");
+        }
         console.log(data);
         return resourceCreated({})
     } catch (e) {
