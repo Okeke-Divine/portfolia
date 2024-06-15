76338fdf05c799c237c8a05821e7b0893dddb6ec
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 11:33:28 +0100

    Updated

--- src/app/api/info/socials/route.js
+++ src/app/api/info/socials/route.js
@@ -1,4 +1,6 @@
+import prisma from "@/app/db";
 import { badRequest, conflict, internalServerError, resourceCreated } from "@/utils/prebuiltApiResponse"
+import { getUserId } from "@/utils/session";
 
 export const POST = async (req) => {
     try {
@@ -8,6 +10,29 @@ export const POST = async (req) => {
         if (!socialType || !socialValue) {
             return badRequest("social type/value is required");
         }
+
+        const userId = await getUserId();
+
+        // check if social type already exists
+        const socialType_exists = await prisma.userSocialInfo.count({
+            where: {
+                userId
+            }
+        })
+
+        if (socialType_exists > 0) {
+            return conflict("Social type already exits");
+        }
+
+        // insert into the data base if social type doesn't exists
+        const socialInfo = await prisma.userSocialInfo.create({
+            data: {
+                name: socialType,
+                value: socialValue
+            }
+        })
+
+
         console.log(data);
         return resourceCreated({})
     } catch (e) {
