411e1f30da02fba45cfb518ad8a731aa776dce73
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-15 21:06:48 +0100

    Updated

--- src/app/api/info/project/AddProject.jsx
+++ src/app/api/info/project/AddProject.jsx
@@ -63,7 +63,7 @@ const AddProject = () => {
               <input
                 type="file"
                 accept="image/*"
-                className="file-input w-full mt-1"
+                className="file-input w-full mt-1 file-input-bordered"
               />
             </div>
             {/* description */}
