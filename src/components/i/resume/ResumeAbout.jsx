3b49efc6ef2ce891ef9c2521536390f1461921fc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:26:50 +0100

    Updated

--- src/components/i/resume/ResumeAbout.jsx
+++ src/components/i/resume/ResumeAbout.jsx
@@ -5,30 +5,8 @@ const ResumeAbout = ({ user }) => {
         <div className="divider divider-start">
           <h3 className="uppercase">About Me</h3>
         </div>
-        <div className="flex gap-2 md:gap-5">
-          {user.profilePicture_url != "" ? (
-            <>
-              <div className="w-fit">
-                <div className="avatar">
-                  <div className="mask mask-squircle bg-green-200 bg-opacity-20 w-52 lg:w-96">
-                    <img
-                      src={
-                        user?.profilePicture_url != ""
-                          ? user.profilePicture_url
-                          : defaultImgUrl
-                      }
-                      alt="User Profile Picture"
-                    />
-                  </div>
-                </div>
-              </div>
-            </>
-          ) : (
-            ""
-          )}
-          <div className="grow">
-            <p className="whitespace-pre-line">{user?.userDetails?.about}</p>
-          </div>
+        <div className="">
+          <p className="whitespace-pre-line">{user?.userDetails?.about}</p>
         </div>
       </div>
     </>
