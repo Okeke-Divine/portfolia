8d7c8557528c93d1423d99143151bf744316f7f8
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 15:51:48 +0100

    Updated

---
+++ src/app/(layout-portfolio)/i/[username]/resume/page.jsx
@@ -0,0 +1,57 @@
+import prisma from "@/app/db";
+import NotFound from "@/app/not-found";
+import { _ucfirst } from "@/utils/main";
+
+export async function generateMetadata({ params }) {
+  const username = params.username;
+  const user = await prisma.user.findFirst({
+    where: { username },
+    select: { fullname: true },
+  });
+  if (!user) {
+    return {
+      title: "404 | Not Found",
+    };
+  }
+  return {
+    title: _ucfirst(user.fullname),
+  };
+}
+
+export default async function Resume({ params }) {
+  const username = params.username;
+  const user = await prisma.user.findFirst({
+    where: { username },
+    select: {
+      id: true,
+      fullname: true,
+      username: true,
+      email: true,
+      profilePicture_url: true,
+      userDetails: {
+        select: {
+          fullname: true,
+          heroTitle: true,
+          profession: true,
+          bio: true,
+          about: true,
+        },
+      },
+    },
+  });
+
+  if (!user) {
+    return (
+      <>
+        <NotFound />
+      </>
+    );
+  }
+
+  return (
+<>
+
+Resume
+    </>
+  );
+}
