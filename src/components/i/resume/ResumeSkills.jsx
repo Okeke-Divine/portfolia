b6e1d62aad8d4124b0b53eb4953c8eb4f0fa4135
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:37:09 +0100

    Updated

--- src/components/i/resume/ResumeSkills.jsx
+++ src/components/i/resume/ResumeSkills.jsx
@@ -1,17 +1,23 @@
 import prisma from "@/app/db";
 
 const ResumeSkills = async ({ userId }) => {
-    const skills = await prisma.userSkills.findMany({
-      where: { userId },
-    });
-  
+  const skills = await prisma.userSkills.findMany({
+    where: { userId },
+  });
+
   return (
     <>
       <div className="app-resume-padding">
         <div className="divider divider-start">
           <h3 className="uppercase">Skills</h3>
         </div>
-        <div className="">Html,css...</div>
+        <div className="flex flex-wrap gap-2 md:gap-5">
+          {skills.map((skill, index) => (
+            <div key={index} className="badge badge-outline">
+              {skill.name}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
