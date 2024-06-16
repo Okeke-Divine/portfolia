121ca74a12318a6b1f53ea71407118fb12e9d36d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:24:34 +0100

    U[pdated

--- src/components/i/resume/ResumeAbout.jsx
+++ src/components/i/resume/ResumeAbout.jsx
@@ -4,9 +4,9 @@ const ResumeAbout = ({ user }) => {
       <div className="app-resume-padding">
         <div className="divider divider-start">
           <h3 className="uppercase">About Me</h3>
-          <div>
-            <p className="whitespace-pre-line">{user?.userDetails?.about}</p>
-          </div>
+        </div>
+        <div>
+          <p className="whitespace-pre-line">{user?.userDetails?.about}</p>
         </div>
       </div>
     </>
