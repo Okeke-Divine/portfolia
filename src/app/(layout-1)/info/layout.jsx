c667059c186dea0744d322850fbf117a25f3d532
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 20:30:56 +0100

    Updated

--- src/app/(layout-1)/info/layout.jsx
+++ src/app/(layout-1)/info/layout.jsx
@@ -12,17 +12,17 @@ export default function InfoLayout({ children }) {
         </div>
         <div className="divider"></div>
         <div>
-          <div className="flex flex-col md:flex-row gap-2">
+          {/* <div className="block md:flex md:flex-row gap-2"> */}
             <div className="w-fit">
-              <div className="max-w-full overflow-x-auto">
-                <div className="join join-horizontal md:join-vertical border-2 overflow-auto">
+              {/* <div className="max-w-full overflow-x-auto"> */}
+                <div className="join join-horizontal md:join-vertical border-2 max-w-[50%] overflow-hidden bg-red-200">
                   <InfoSideNav />
                 </div>
-              </div>
+              {/* </div> */}
             </div>
             {/*  */}
             <div className="w-full">{children}</div>
-          </div>
+          {/* </div> */}
         </div>
       </div>
     </>
