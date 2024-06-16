a94d8b088c7622ac4ea0d5e0b83584bcd6a22680
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:39:19 +0100

    Updated

--- src/components/i/resume/ResumeSkills.jsx
+++ src/components/i/resume/ResumeSkills.jsx
@@ -7,18 +7,24 @@ const ResumeSkills = async ({ userId }) => {
 
   return (
     <>
-      <div className="app-resume-padding">
-        <div className="divider divider-start">
-          <h3 className="uppercase">Skills</h3>
-        </div>
-        <div className="flex flex-wrap gap-x-2 md:gap-x-3 gap-y-2">
-          {skills.map((skill, index) => (
-            <div key={index} className="badge badge-outline badge-md">
-              {skill.skill_name}
+      {skills.length > 0 ? (
+        <>
+          <div className="app-resume-padding">
+            <div className="divider divider-start">
+              <h3 className="uppercase">Skills</h3>
             </div>
-          ))}
-        </div>
-      </div>
+            <div className="flex flex-wrap gap-x-2 md:gap-x-3 gap-y-2">
+              {skills.map((skill, index) => (
+                <div key={index} className="badge badge-outline badge-md">
+                  {skill.skill_name}
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 };
