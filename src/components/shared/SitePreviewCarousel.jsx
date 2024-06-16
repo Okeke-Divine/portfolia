fa25938a8291d736a948c2c93c680fc692486a07
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-17 00:17:25 +0100

    Updated

--- src/components/shared/SitePreviewCarousel.jsx
+++ src/components/shared/SitePreviewCarousel.jsx
@@ -1,6 +1,16 @@
+"use client";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
+const images = [
+  "iPad-Air-4-localhost (1).png",
+  "Samsung-Galaxy-S20-localhost.png",
+  "Macbook-Air-localhost.png",
+  "Macbook-Air-localhost (1).png",
+  "iPhone-13-PRO-localhost.png",
+  "iPad-Air-4-localhost.png",
+];
 
 const SitePreviewCarousel = () => {
   const settings = {
@@ -15,10 +25,13 @@ const SitePreviewCarousel = () => {
 
   return (
     <>
-      <div>
+      <div className="fixed top-0 left-0 w-full h-[100vh] bg-red-300">
         <Slider {...settings}>
           <div>
             <img src="/images/Samsung-Galaxy-S20-localhost.png" />
+          </div>
+          <div>
+            <img src="/images/iPad-Air-4-localhost (1).png" />
           </div>
         </Slider>
       </div>
