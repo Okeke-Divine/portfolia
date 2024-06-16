342d51ec7a3975bbc49bda773255d5c21200a4dc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 19:11:48 +0100

    Updated

--- src/constants/shared/constant.js
+++ src/constants/shared/constant.js
@@ -1,4 +1,4 @@
-export const defaultImgUrl2 = "/images/avatar/avatar1.png"
+export const defaultImgUrl2 = process.env.NEXT_PUBLIC_BASE_URL+"/images/avatar/avatar1.png"
 export const defaultImgUrl = "/images/avatar/avatar1.png"
 export const twitterAccUrl = "https://twitter.com/okekedivine__";
 export const buyMeACoffeUrl = "https://buymeacoffee.com/divinho";
