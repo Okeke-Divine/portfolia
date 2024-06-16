d21518383a49d9d3b1fdbb969fb1615a9196be1e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:35:10 +0100

    Updated

---
+++ src/components/i/resume/ResumeSkills.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ResumeSkills = ({ user }) => {
+  return (
+    <>
+      <div className="app-resume-padding">
+        <div className="divider divider-start">
+          <h3 className="uppercase">Skills</h3>
+        </div>
+        <div className="">Html,css...</div>
+      </div>
+    </>
+  );
+};
+
+export default ResumeSkills;
