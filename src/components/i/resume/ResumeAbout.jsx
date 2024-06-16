85af33d234de812a99ae50f564852784019a6a1d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:20:26 +0100

    Updated

--- src/components/i/resume/ResumeAbout.jsx
+++ src/components/i/resume/ResumeAbout.jsx
@@ -1,7 +1,11 @@
 const ResumeAbout = ({ user }) => {
   return (
     <>
-      <div className="app-resume-padding"></div>
+      <div className="app-resume-padding">
+        <div className="divider">
+          <h2 className="uppercase">About Me</h2>
+        </div>
+      </div>
     </>
   );
 };
