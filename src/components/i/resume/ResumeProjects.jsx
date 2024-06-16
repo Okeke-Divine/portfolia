4203b67160026e3ac10ff3f1c97e3ce526c5d6b7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:41:41 +0100

    Updated

--- src/components/i/resume/ResumeProjects.jsx
+++ src/components/i/resume/ResumeProjects.jsx
@@ -15,11 +15,17 @@ const ResumeProjects = async ({ userId }) => {
 
   return (
     <>
-      <div className="app-resume-padding">
-        <div className="divider divider-start">
-          <h3 className="uppercase">Projects</h3>
-        </div>
-      </div>
+      {projects.length > 0 ? (
+        <>
+          <div className="app-resume-padding">
+            <div className="divider divider-start">
+              <h3 className="uppercase">Projects</h3>
+            </div>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 };
