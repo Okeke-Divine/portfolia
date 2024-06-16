6b4bb973ce6a831ba9cb3a1a62deb84812de9462
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 13:55:49 +0100

    Updated

--- src/app/api/info/project/delete/route.js
+++ src/app/api/info/project/delete/route.js
@@ -1,11 +1,28 @@
+import prisma from "@/app/db";
 import { internalServerError, resourceDeleted } from "@/utils/prebuiltApiResponse"
 
 export const POST = async (req) => {
-    try{
+    try {
         const data = await req.json();
         console.log(data);
-        return resourceDeleted({});
-    }catch(e){
+
+        const { project_id } = data;
+
+        if (!project_id || !parseInt(project_id)) {
+            return badRequest("project id required/invalid project id")
+        }
+
+        const delete_project = await prisma.userProjects.delete({
+            where: {
+                id: project_id
+            }
+        })
+        if (delete_project) {
+            return resourceDeleted();
+        }
+
+        return internalServerError("failed to delete a social skill")
+    } catch (e) {
         return internalServerError(e)
     }
 }
