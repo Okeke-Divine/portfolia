8db54a9ad946e1c0c5583234bee25058d970baa3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:51:02 +0100

    Updated

--- src/components/i/resume/ResumeProjects.jsx
+++ src/components/i/resume/ResumeProjects.jsx
@@ -32,7 +32,7 @@ const ResumeProjects = async ({ userId }) => {
                   {/* project tags */}
                   {project.tags !== "" ? (
                     <>
-                      <div className="mb-2">
+                      <div className="mb-2 text-gray-200">
                         {project.tags.split(",").map((tag, index) => (
                           <span key={index}>{tag.trim()}, </span>
                         ))}
