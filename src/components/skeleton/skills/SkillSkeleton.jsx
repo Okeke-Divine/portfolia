5ba14b8c217d4d59615478ecb8842ed796436b0c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 13:49:33 +0100

    Updated

--- src/components/skeleton/skills/SkillSkeleton.jsx
+++ src/components/skeleton/skills/SkillSkeleton.jsx
@@ -1,14 +1,8 @@
 const SkillSkeleton = () => {
   return (
     <>
-      <div className="skeleton w-full h-10 rounded-md mb-1"></div>
       {[1, 1, 1, 1, 1, 1, 1].map((item, index) => (
-        <>
-          <div
-            key={index}
-            className="skeleton w-full h-10 rounded-md mb-1"
-          ></div>
-        </>
+        <div key={index} className="skeleton w-full h-10 rounded-md mb-1"></div>
       ))}
     </>
   );
