c4003ebe329f7079c43058c6e9c6471abeb3fab1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:13:08 +0100

    Updated

--- src/components/i/resume/ResumeSocialLink.jsx
+++ src/components/i/resume/ResumeSocialLink.jsx
@@ -20,27 +20,31 @@ const ResumeSocialLink = async ({ userId }) => {
 
   return (
     <>
-      <div className="flex gap-2 flex-wrap">
-        {social_link.map((link, index) => (
-          <Link
-            href={
-              link.name == "email"
-                ? "mailto:" + link.value
-                : link.name == "phone_number"
-                ? "tel:" + link.value
-                : link.name == "whatsapp"
-                ? "https://api.whatsapp.com/?phone=" + link.value
-                : link.value
-            }
-            target="_blank"
-            key={index}
-            className="app-portfolio-navbar-link"
-          >
-            <i
-              className={`${getIconClass(link.name)} app-portfolio-navbar-icon`}
-            ></i>
-          </Link>
-        ))}
+      <div className="app-resume-padding">
+        <div className="flex gap-2 flex-wrap">
+          {social_link.map((link, index) => (
+            <Link
+              href={
+                link.name == "email"
+                  ? "mailto:" + link.value
+                  : link.name == "phone_number"
+                  ? "tel:" + link.value
+                  : link.name == "whatsapp"
+                  ? "https://api.whatsapp.com/?phone=" + link.value
+                  : link.value
+              }
+              target="_blank"
+              key={index}
+              className="app-portfolio-navbar-link"
+            >
+              <i
+                className={`${getIconClass(
+                  link.name
+                )} app-portfolio-navbar-icon`}
+              ></i>
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
