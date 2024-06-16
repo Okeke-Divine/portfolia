b414130957e9289c93bcdad70a949ef8fcfb69b4
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 22:17:02 +0100

    Updated

--- src/components/shared/ApplicationSidebar.jsx
+++ src/components/shared/ApplicationSidebar.jsx
@@ -129,9 +129,18 @@ export default function ApplicationSidedar() {
           isSidebarExpanded ? "left-0" : "-left-full"
         } md:left-0 h-[100%] max-h-[100vh] overflow-y-auto bg-black w-[250px] flex justify-between flex-col text-white px-5 md:px-7 py-5 md:py-10 duration-300 z-50`}
       >
-        <div className="text-2xl text-center font-bold uppercase">
-          {config.app_name}
+        <div>
+          <div>
+            <img
+              src="/images/logo/logo.png"
+              alt={config.app_name + "'s logo"}
+            />
+          </div>
+          <div className="text-2xl text-center font-bold uppercase">
+            {config.app_name}
+          </div>
         </div>
+        {/* // */}
         <div>
           {links.map((link, index) => (
             <Link
