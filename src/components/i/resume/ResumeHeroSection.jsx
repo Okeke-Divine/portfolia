4eb8cc0718987c8c8b2e6caa30d6687dbfc9758b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:10:48 +0100

    Updated

--- src/components/i/resume/ResumeHeroSection.jsx
+++ src/components/i/resume/ResumeHeroSection.jsx
@@ -3,10 +3,10 @@ const ResumeHeroSection = ({ user }) => {
     <>
       <div className="app-resume-padding">
         <div>
-          <h1 className="text-5xl">Okeke Divine-Vessel.</h1>
+          <h1 className="text-5xl">{user?.userDetails?.fullname}.</h1>
         </div>
         <div className="flex gap-2 items-center">
-          <div className="w-fit text-xl">Full Stack Web Developer @ Google</div>
+          <div className="w-fit text-xl">{user?.userDetails?.profession}</div>
           <div className="grow">
             <div className="divider"></div>
           </div>
