5759e68f0d135e313d0c01e2296947ab8613a3c5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 19:22:56 +0100

    Updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -42,12 +42,6 @@ export const metadata = {
   },
   // icons: {
   //   icon: '/icon.png',
-  //   shortcut: '/shortcut-icon.png',
-  //   apple: '/apple-icon.png',
-  //   other: {
-  //     rel: 'apple-touch-icon-precomposed',
-  //     url: '/apple-touch-icon-precomposed.png',
-  //   },
   // },
 };
 
