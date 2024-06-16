ee8a1445cc4532614cf0a5f44c6eba201af00104
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 20:19:40 +0100

    Updated

---
+++ src/app/(layout-1)/resume/builder/layout.jsx
@@ -0,0 +1,53 @@
+import { getCurrentSession } from "@/utils/session";
+import Link from "next/link";
+
+const links = [{ name: "Preview" }];
+
+const LayoutBuilderLayout = async ({ children }) => {
+  const session = await getCurrentSession();
+
+  return (
+    <>
+      <div>
+        <div>
+          <h1 className="flex items-center gap-2">
+            <i className="fi fi-tr-poll-h titleIcon"></i> Resume Builder
+          </h1>
+          <p className="app-text-light">
+            No code Resume builder |{" "}
+            <Link
+              href={"/i/" + session.user.username + "/resume"}
+              target="_blank"
+              className="app-link-primary font-bold"
+            >
+              Live Preview
+              <i className="fi fi-tr-arrow-up-right-from-square text-[15px] pl-1"></i>
+            </Link>
+          </p>
+        </div>
+        <div className="divider"></div>
+        <div>
+          <div className="flex flex-col md:flex-row gap-2">
+            <div className="w-[100px">
+              <div className="join join-horizontal md:join-vertical border-2 w-fit">
+                {links.map((link, index) => (
+                  <Link
+                    key={index}
+                    href="#"
+                    className="btn join-item font-semibold hover:app-bg-primary-dark app-bg-primary text-white"
+                  >
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+            {/* <div className="divider divider-vertical"></div> */}
+            <div className="grow">{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LayoutBuilderLayout;
