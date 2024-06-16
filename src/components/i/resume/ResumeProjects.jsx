01f369ccab6a928c1f7425caca94af241fc29ed3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-16 16:38:52 +0100

    Updated

---
+++ src/components/i/resume/ResumeProjects.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResumeProjects = () => {
+  return (
+    <>
+      <div className="app-resume-padding">d</div>
+    </>
+  );
+};
+
+export default ResumeProjects;
