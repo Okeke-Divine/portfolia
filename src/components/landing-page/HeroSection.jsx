30207f45deb1a916ee0dd1dd0267758d60f670cc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 15:06:29 +0100

    Updated

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -1,11 +1,14 @@
+import Link from "next/link"
+
 const HeroSection = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 landingPagePadx py-5">
         <div>
-          <h1>
+          <h1 className="text-5xl">
             Make a beautiful portfolio + resume in <span className="app-text-primary">minutes</span> not days
           </h1>
+          <Link className="app-primary-button" href="/signup">Get Started → It's free</Link>
         </div>
         <div></div>
       </div>
