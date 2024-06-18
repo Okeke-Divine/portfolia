9e4691809a4654f851a46827fd5b6787abcbfed8
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 14:39:39 +0100

    Updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import FAQ from "@/components/landing-page/FAQ";
+import Features from "@/components/landing-page/Features";
+import HeroSection from "@/components/landing-page/HeroSection";
+import HowItWorks from "@/components/landing-page/HowItWorks";
+import LowerCallToAction from "@/components/landing-page/LowerCallToAction";
+import Reviews from "@/components/landing-page/Reviews";
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 
 export default function Home() {
   return (
- <>
-  test
- </>
+    <>
+      <Navbar />
+      <HeroSection />
+      <HowItWorks />
+      <Features />
+      <Reviews />
+      <FAQ />
+      <LowerCallToAction />
+      <Footer />
+    </>
   );
 }
---
+++ src/components/landing-page/LowerCallToAction.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LowerCallToAction = () => {
+  return (
+    <div>LowerCallToAction</div>
+  )
+}
+
+export default LowerCallToAction
