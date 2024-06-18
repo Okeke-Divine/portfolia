47c06c90ae03c073be8147d8b52a7d581b22cd3e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 15:15:18 +0100

    Updated

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 const HeroSection = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 landingPagePadx py-5 md:py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 landingPagePadx py-5 md:py-10">
         <div>
           <h1 className="text-4xl md:text-5xl">
             Make a beautiful portfolio + resume in <span className="app-text-primary">minutes</span> not <span className="line-through">days</span>
@@ -16,7 +16,13 @@ const HeroSection = () => {
           <Link className="app-primary-button mt-3 w-fit" href="/signup">Get Started → It's free</Link>
         </div>
         <div className="">
-
+          <div className="w-full">
+            <img
+              src={imageThumbnail}
+              alt={config.app_name}
+              class="object-cover w-full h-full"
+            />
+          </div>
         </div>
       </div>
     </>
