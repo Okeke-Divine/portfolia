f7ad719828537c0b520294c25cf53884a586d028
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 14:45:17 +0100

    Updated

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,9 +1,19 @@
+import config from "@/data/config.json"
 
 const Footer = () => {
   return (
     <>
       <div className="footer bg-base-200 py-2 md:py-5 landingPagePadx">
-        <div></div>
+        <div>
+          <div className="flex justify-center">
+            <img
+              className="w-24 mb-2 rounded-full shadow-lg"
+              src="/images/logo/logo.png"
+              alt={config.app_name + "'s logo"}
+            />
+          </div>
+          <div className="uppercase text-xl font-bold">{config.app_name}</div>
+        </div>
         <div>
           <h6 className="footer-title">Links</h6>
         </div>
