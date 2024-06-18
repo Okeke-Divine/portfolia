35df3d87171b6e8f2a4884b4fdae9895009020a8
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 21:43:55 +0100

    Updated

--- src/components/landing-page/FAQ.jsx
+++ src/components/landing-page/FAQ.jsx
@@ -5,32 +5,34 @@ const faqs = [
 const FAQ = () => {
   return (
     <>
-      <div className="landingPagePadx join join-vertical w-full py-5">
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" defaultChecked />
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
+      <div className="landingPagePadx flex justify-center">
+        <div className=" join join-vertical w-full py-5 md:max-w-[400px]">
+          <div className="collapse collapse-arrow join-item border border-base-300 ">
+            <input type="radio" name="my-accordion-4" defaultChecked />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
           </div>
-          <div className="collapse-content">
-            <p>hello</p>
+          <div className="collapse collapse-arrow join-item border border-base-300">
+            <input type="radio" name="my-accordion-4" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
           </div>
-        </div>
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
-          </div>
-          <div className="collapse-content">
-            <p>hello</p>
-          </div>
-        </div>
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
-          </div>
-          <div className="collapse-content">
-            <p>hello</p>
+          <div className="collapse collapse-arrow join-item border border-base-300">
+            <input type="radio" name="my-accordion-4" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
           </div>
         </div>
       </div>
