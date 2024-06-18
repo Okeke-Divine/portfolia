270f8128802d14c1b4702746fea9006b7e2c53c5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 14:41:43 +0100

    Updated

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,9 +2,11 @@
 const Footer = () => {
   return (
     <>
-      <div className="footer">
+      <div className="footer bg-base-200 p-3 md:p-5 lg:p-8">
         <div></div>
-        <div></div>
+        <div>
+          <h6 className="footer-title">Links</h6>
+        </div>
         <div></div>
         <div></div>
       </div>
