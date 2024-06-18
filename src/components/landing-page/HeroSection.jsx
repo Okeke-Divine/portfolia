3e87932ee968609380214f0bc3ed3daa434bd561
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 15:17:13 +0100

    Updated

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
             <img
               src={"/images/mockups/923_1x_shots_so.png"}
               alt={config.app_name}
-              class="object-cover w-full h-full"
+              class="object-cover w-full rounded-md"
             />
           </div>
         </div>
