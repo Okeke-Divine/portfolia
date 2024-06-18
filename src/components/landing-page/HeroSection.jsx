dd0bc5c81a477d92ccff0db3cf0a449ddfb923eb
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 15:04:28 +0100

    Updated

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -2,10 +2,13 @@ const HeroSection = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 landingPagePadx">
-    <div>
-        Make a beautiful portfolio + resume in <span className="app-text-primary">minutes</span> not days
-    </div>
-    <div></div>
+        <div>
+          <h1>
+            Make a beautiful portfolio + resume in <span className="app-text-primary">minutes</span> not days
+
+          </h1>
+        </div>
+        <div></div>
       </div>
     </>
   )
