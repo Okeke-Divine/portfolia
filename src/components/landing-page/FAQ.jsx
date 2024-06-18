eef4858f9878309a4de6224038be58f1a5a6c99f
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-18 21:47:39 +0100

    Updated

--- src/components/landing-page/FAQ.jsx
+++ src/components/landing-page/FAQ.jsx
@@ -9,7 +9,7 @@ const FAQ = () => {
         <div className="text-center uppercase app-text-primary">FAQs</div>
         <h2 className="text-center">Questions & Anwers</h2>
         <div className="flex justify-center">
-          <div className="text-center text-gray-600 md:max-w-[60%]">Any other questions? Reach out on twitter or mail. You will get answers withing a few hours.</div>
+          <div className="text-center text-gray-600 md:max-w-[60%] lg:max-w-[40%]">Any other questions? Reach out on twitter or mail. You will get answers withing a few hours.</div>
         </div>
         <div className=" flex justify-center">
           <div className=" join join-vertical w-full py-5 md:max-w-[80%] lg:max-w-[70%]">
