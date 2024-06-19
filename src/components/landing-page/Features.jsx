5f84bd65a9877bb64c819cb71380f323c33cb81b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-19 12:37:20 +0100

    Udpated

--- src/components/landing-page/Features.jsx
+++ src/components/landing-page/Features.jsx
@@ -1,24 +1,24 @@
 
 const features = [
-  { message: "Non-IT people can finally build this amazing landing page on their own (like me) 🤩 Truly brilliant idea to save lots of time and money for busy solo entrepreneurs! ", name: "John Doe", username: "@johndoe", img: "https://makelanding.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimonial-wonji.96376f67.jpeg&w=48&q=75" },
-  { message: "Non-IT people can finally build this amazing landing page on their own (like me) 🤩 Truly brilliant idea to save lots of time and money for busy solo entrepreneurs! ", name: "John Doe", username: "@johndoe", img: "https://makelanding.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimonial-wonji.96376f67.jpeg&w=48&q=75" },
-  { message: "Non-IT people can finally build this amazing landing page on their own (like me) 🤩 Truly brilliant idea to save lots of time and money for busy solo entrepreneurs! ", name: "John Doe", username: "@johndoe", img: "https://makelanding.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimonial-wonji.96376f67.jpeg&w=48&q=75" },
-  { message: "Non-IT people can finally build this amazing landing page on their own (like me) 🤩 Truly brilliant idea to save lots of time and money for busy solo entrepreneurs! ", name: "John Doe", username: "@johndoe", img: "https://makelanding.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimonial-wonji.96376f67.jpeg&w=48&q=75" },
-  { message: "Non-IT people can finally build this amazing landing page on their own (like me) 🤩 Truly brilliant idea to save lots of time and money for busy solo entrepreneurs! ", name: "John Doe", username: "@johndoe", img: "https://makelanding.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimonial-wonji.96376f67.jpeg&w=48&q=75" },
-  { message: "Non-IT people can finally build this amazing landing page on their own (like me) 🤩 Truly brilliant idea to save lots of time and money for busy solo entrepreneurs! ", name: "John Doe", username: "@johndoe", img: "https://makelanding.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimonial-wonji.96376f67.jpeg&w=48&q=75" },
-  { message: "Non-IT people can finally build this amazing landing page on their own (like me) 🤩 Truly brilliant idea to save lots of time and money for busy solo entrepreneurs! ", name: "John Doe", username: "@johndoe", img: "https://makelanding.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimonial-wonji.96376f67.jpeg&w=48&q=75" },
+  { title: "Inbuilt SEO", message: "Our Artificial Intelligence uses millions of converting headlines to write yours. It speaks 50+ languages!", icon: "" },
 ]
 
 const Features = () => {
   return (
     <>
-      <secton className="landingPagePadx py-5 md:p-10">
+      <secton className="landingPagePadx py-5 md:p-10 bg-base-200">
         <div className="text-center uppercase app-text-primary">Features</div>
         <h2 className="text-center">The fastest way to setup a portfolio + resume</h2>
         <div className="flex justify-center mt-2">
           <div className="text-center text-gray-600 max-w-[70%] md:max-w-[60%] lg:max-w-[40%]">Packed with a ton of featueres</div>
         </div>
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap">
+          {features.map((feature, index) => (
+            <div key={index} className="p-5 bg-white">
+              <div className="text-center text-xl font-bold">{feature.title}</div>
+              <div className="text-gray-500">{feature.message}</div>
+            </div>
+          ))}
         </div>
       </secton>
     </>
