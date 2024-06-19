d7597941255de980968ad2fddbc96a9d9821e6e8
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-19 11:48:33 +0100

    Updated

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -20,8 +20,9 @@ const HeroSection = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad inventore quos ab ducimus aliquam soluta nam quod doloremque deleniti quas. Maiores iusto ipsum enim ipsam corrupti neque voluptates amet totam?
           </p>
           <Link className="app-primary-button mt-3 w-fit" href="/signup">Get yours now → It's free</Link>
-          <div className="mt-2">
-            <img src="/images/svg/flowe-left.svg" alt="flower left" />
+          <div className="mt-2 flex items-center">
+            <img src="/images/svg/flower-left.svg" alt="flower left" />
+            <img src="/images/svg/flower-right.svg" alt="flower right" />
           </div>
         </div>
         <div className="">
