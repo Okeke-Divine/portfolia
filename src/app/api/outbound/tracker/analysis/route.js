87c0c5967e2854512c83bb9f62e94b843f9b719b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 00:34:18 +0100

    Updated

--- src/app/api/outbound/tracker/analysis/route.js
+++ src/app/api/outbound/tracker/analysis/route.js
@@ -14,11 +14,17 @@ export const POST = async (req) => {
         }
 
         if (actionType == "portfolioView") {
+
+            // check if user exist
             const user = await prisma.user.findFirst({ where: { username }, select: { id: true } });
+
             if (user !== null) {
+                // get the user id
                 const userId = user.id;
+                //get the previeous count
                 const portfolioViewCount = await prisma.userAnalytics.findFirst({ where: { userId }, select: { portfolioViewCount: true } }) || 0;
                 console.log(portfolioViewCount);
+                // upsert the previous count or create a new row
                 const updatePortfolioViewCount = await prisma.userAnalytics.upsert({
                     where: { userId },
                     update: { portfolioViewCount: portfolioViewCount + 1 },
