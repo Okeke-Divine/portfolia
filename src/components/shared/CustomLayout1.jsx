a10c070bf32030003fb145d7a4aa181d89acb05d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 00:12:21 +0100

    Updated

--- src/components/shared/CustomLayout1.jsx
+++ src/components/shared/CustomLayout1.jsx
@@ -1,12 +1,11 @@
-import Link from "next/link"
+import BackButton from "./BackButton"
 
 const CustomLayout1 = ({ children }) => {
     return (
         <>
             <div className="max-w-xl mx-auto py-5 md:py-10">
                 <div>
-                    <Link href=".." className="uppercase font-bold">
-                        <i className="fi flaticon-offset fi-tr-angle-small-left"></i> Back</Link>
+                    <BackButton />
                 </div>
                 <div className="divider"></div>
                 <div>{children}</div>
