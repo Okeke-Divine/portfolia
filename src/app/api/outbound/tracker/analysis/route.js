5e7f31b814f9cf468f982a03b1f622443260d095
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 00:44:07 +0100

    UPDATED

--- src/app/api/outbound/tracker/analysis/route.js
+++ src/app/api/outbound/tracker/analysis/route.js
@@ -5,12 +5,14 @@ import { userAgent } from "next/server";
 export const POST = async (req) => {
     try {
         const data = await req.json();
+        console.log(data);
         const { username, actionType } = data;
 
         if (!username || !actionType) {
             return resourceLoaded({});
         }
 
+        //portfolioView
         if (actionType === "portfolioView") {
             // Check if user exists
             const user = await prisma.user.findFirst({ where: { username }, select: { id: true } });
@@ -33,6 +35,34 @@ export const POST = async (req) => {
                     update: { portfolioViewCount: portfolioViewCount + 1 },
                     create: {
                         userId, portfolioViewCount: 1
+                    }
+                });
+            }
+        }
+
+        //resumeView
+        if (actionType === "resumeView") {
+            // Check if user exists
+            const user = await prisma.user.findFirst({ where: { username }, select: { id: true } });
+
+            if (user !== null) {
+                // Get the user id
+                const userId = user.id;
+
+                // Get the previous count
+                const userAnalytics = await prisma.userAnalytics.findFirst({
+                    where: { userId },
+                    select: { resumeViewCount: true }
+                });
+
+                const resumeViewCount = userAnalytics ? userAnalytics.resumeViewCount : 0;
+
+                // Upsert the previous count or create a new record
+                await prisma.userAnalytics.upsert({
+                    where: { userId },
+                    update: { resumeViewCount: resumeViewCount + 1 },
+                    create: {
+                        userId, resumeViewCount: 1
                     }
                 });
             }
