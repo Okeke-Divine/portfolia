50bd659ba6826b3dd7c1a0dc5d1f33e2896465a5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 00:51:26 +0100

    Updated

---
+++ src/components/shared/AnalyticWatcher.jsx
@@ -0,0 +1,15 @@
+"use client"
+import { useEffect } from "react"
+
+const AnalyticWatcher = () => {
+
+    useEffect(function (){
+        
+    })
+
+    return (
+        <></>)
+
+}
+
+export default AnalyticWatcher
