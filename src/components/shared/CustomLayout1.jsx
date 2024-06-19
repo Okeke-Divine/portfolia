e4422d7bc18937de3320177fe29ef7d647f15332
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-19 21:54:53 +0100

    Updated

--- src/components/shared/CustomLayout1.jsx
+++ src/components/shared/CustomLayout1.jsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 const CustomLayout1 = ({ children }) => {
     return (
         <>
             <div className="max-w-xl mx-auto">
+                <div>
+                    <Link href="..">
+                        <i className="fi flaticon-offset"></i>
+                        Back</Link>
+                </div>
                 <div>{children}</div>
             </div>
         </>
