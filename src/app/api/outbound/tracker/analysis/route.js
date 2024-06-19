6d8529937b3c145ccb3291bf39e2c7607fe496cc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 00:28:11 +0100

    Updated

--- src/app/api/outbound/tracker/analysis/route.js
+++ src/app/api/outbound/tracker/analysis/route.js
@@ -3,7 +3,14 @@ import { internalServerError, resourceLoaded } from "@/utils/prebuiltApiResponse
 export const POST = async (req) => {
     try {
         const data = await req.json()
-        console.log(data)
+        //destructure
+        const username = data.username;
+        const actionType = data.actionType;
+
+        if (!username || !actionType) {
+            return resourceLoaded({});
+        }
+
         return resourceLoaded({})
     } catch (e) {
         return internalServerError(e)
