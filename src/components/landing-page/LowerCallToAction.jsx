75356a6b61f6720b65e0dcfffd47574810345ec3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-19 11:44:09 +0100

    Updated

--- src/components/landing-page/LowerCallToAction.jsx
+++ src/components/landing-page/LowerCallToAction.jsx
@@ -4,7 +4,7 @@ const LowerCallToAction = () => {
     <>
       <div className="bg-base-300 py-5 md:py-10">
         <div className="landingPagePadx">
-          <div className="text-center font-bold text-3xl">Launc your portfolio today</div>
+          <div className="text-center font-bold text-3xl">Launch your portfolio today</div>
           <div className="py-3 text-center">Just fill in your info and your portfolio + resume will be automatically generated</div>
           <div className="join flex justify-center mt-1">
             <input type="text" placeholder="Your username" className="join-item input input-bordered" />
