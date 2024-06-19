27e11a024d762e5543d2a220212692cc90458008
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-19 21:55:34 +0100

    Updated

--- src/components/shared/CustomLayout1.jsx
+++ src/components/shared/CustomLayout1.jsx
@@ -3,11 +3,10 @@ import Link from "next/link"
 const CustomLayout1 = ({ children }) => {
     return (
         <>
-            <div className="max-w-xl mx-auto">
+            <div className="max-w-xl mx-auto py-5 md:py-10">
                 <div>
-                    <Link href="..">
-                        <i className="fi flaticon-offset"></i>
-                        Back</Link>
+                    <Link href=".." className="uppercase font-bold">
+                        <i className="fi flaticon-offset"></i> Back</Link>
                 </div>
                 <div>{children}</div>
             </div>
