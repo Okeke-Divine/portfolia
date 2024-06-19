62980d1714d1f8b195a8d6338aa959ffe9b91748
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 00:53:32 +0100

    Updated

--- src/components/shared/AnalyticWatcher.jsx
+++ src/components/shared/AnalyticWatcher.jsx
@@ -1,10 +1,23 @@
 "use client"
 import { useEffect } from "react"
 
-const AnalyticWatcher = ({parent}) => {
+const AnalyticWatcher = ({ parent }) => {
 
-    useEffect(function (){
-        
+    useEffect(function () {
+
+        // Get all elements with the .clickMeCounter class
+        const elements = document.querySelectorAll(".clickMeCounter");
+
+        // Add event listeners to all elements
+        elements.forEach(element => {
+            element.addEventListener("click", handleClick);
+        });
+
+        return () => {
+            elements.forEach(element => {
+                element.removeEventListener("click", handleClick);
+            });
+        }
     })
 
     return (
