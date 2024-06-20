302b3dfee4c1b39de629a1396489093a46786c93
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 01:31:26 +0100

    Updated

---
+++ src/components/i/resume/ResumeDownload.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+const ResumeDownload = () => {
+    return (
+        <>
+            <button className="fixed bottom-5 left-5">
+                <i class="fi fi-tr-cloud-download-alt text-4xl"></i>
+            </button>
+        </>
+    )
+}
+
+export default ResumeDownload
