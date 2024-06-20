af70e062982a0315cda74220475cec3e96925f54
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 15:34:55 +0100

    Updated

--- src/app/(layout-1)/info/language/page.jsx
+++ src/app/(layout-1)/info/language/page.jsx
@@ -1,13 +1,18 @@
+import AddLanguage from "@/components/info/language/AddLanguage";
+import ViewLanguages from "@/components/info/language/ViewLanguages";
+
 export const metadata = {
   title: "Language | Info",
 };
 
 const LanguageInfo = () => {
   return (
-<>
-    
-</>
-    )
+    <>
+      <AddLanguage />
+      <div className="divider"></div>
+      <ViewLanguages />
+    </>
+  )
 }
 
 export default LanguageInfo
---
+++ src/components/info/language/ViewLanguages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewLanguages = () => {
+  return (
+    <div>ViewLanguages</div>
+  )
+}
+
+export default ViewLanguages
