0cc390e4cd9e76f1546b2cc66fc0daa23484fbca
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-21 00:33:00 +0100

    Updated

--- src/components/i/resume/ResumeLanguage.jsx
+++ src/components/i/resume/ResumeLanguage.jsx
@@ -2,12 +2,29 @@ import prisma from "@/app/db";
 
 const ResumeLanguage = async ({ userId }) => {
 
-    const skills = await prisma.userLanguage.findMany({
+    const languages = await prisma.userLanguage.findMany({
         where: { userId },
     });
 
     return (
-        <div>ResumeLanguage</div>
+        <>
+            {languages.length > 0 ? (
+                <>
+                    <div className="app-resume-padding app-resume-container-margin-top">
+                        <div className="divider divider-start">
+                            <h3 className="uppercase">Languages</h3>
+                        </div>
+                        <div className="flex flex-wrap gap-x-2 md:gap-x-3 gap-y-2">
+                            {languages.map((language, index) => (
+                                <div key={index} className="badge badge-outline badge-md">
+                                    {language.name} - {language.proficiency}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            ) : ""}
+        </>
     )
 }
 
