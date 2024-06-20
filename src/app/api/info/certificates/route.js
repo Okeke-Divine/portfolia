877a36a3f811ea90d209b8423f86a3cb606c87bf
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 18:43:14 +0100

    Updated

---
+++ src/app/api/info/certificates/route.js
@@ -0,0 +1,58 @@
+import prisma from "@/app/db";
+import { badRequest, conflict, internalServerError, resourceCreated, resourceLoaded } from "@/utils/prebuiltApiResponse"
+import { getUserId } from "@/utils/session";
+
+export const GET = async () => {
+    try {
+        const userId = await getUserId()
+        const certificates = await prisma.userCertificate.findMany({
+            where: {
+                userId,
+            },
+            select: {
+                id: true,
+                name: true,
+                issuer: true,
+                issueDate: true
+            }
+        }) || [];
+        return resourceLoaded(issueDate)
+    } catch (e) {
+        return internalServerError("")
+    }
+}
+
+export const POST = async (req) => {
+    try {
+        const data = await req.json();
+        const { language, proficiency } = data;
+
+        if (!language || !proficiency) {
+            return badRequest("All fields are required");
+        }
+
+        const userId = await getUserId();
+
+        // insert into the data base if social type doesn't exists
+        const inserted_language = await prisma.userLanguage.create({
+            data: {
+                userId,
+                name: language,
+                proficiency
+            },
+            select: {
+                id: true,
+                name: true,
+                proficiency: true
+            }
+        })
+
+        if (inserted_language) {
+            return resourceCreated(inserted_language)
+        } else {
+            return internalServerError("Error inserting language")
+        }
+    } catch (e) {
+        return internalServerError(e)
+    }
+}
