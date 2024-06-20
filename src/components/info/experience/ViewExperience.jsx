aa36949c2a44168890995f5344a35819d9d0bf31
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 20:39:16 +0100

    Updated

--- src/components/info/experience/ViewExperience.jsx
+++ src/components/info/experience/ViewExperience.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { app_color_primary } from "@/constants/shared/color";
+import Swal from "sweetalert2";
+import SkillSkeleton from "@/components/skeleton/skills/SkillSkeleton";
 
 const ViewExperience = () => {
+
+  const [languages, setLanguages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const channel = new BroadcastChannel("user-certificates-channel");
+
+
   return (
     <div>ViewExperience</div>
   )
