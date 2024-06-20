611a146a177726134aa45a618b2a88fcb527739e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 20:12:58 +0100

    Updated

--- src/components/info/language/AddLanguage.jsx
+++ src/components/info/language/AddLanguage.jsx
@@ -61,56 +61,63 @@ const AddLanguage = () => {
 
   return (
     <>
-      <form onSubmit={_addLanguage} className="form-control">
-        {/* language */}
-        <div className="mb-3">
-          <div>
-            <label className="font-bold text-md">Language</label>
-          </div>
-          <div className="input input-bordered flex items-center gap-2 mt-1">
-            <input
-              type="text"
-              className="grow"
-              placeholder="Language"
-              required
-              ref={languageRef}
-            />
-          </div>
-        </div>
-        {/* proficiency */}
-        <div className="mb-3">
-          <div>
-            <label className="font-bold text-md">Proficiency</label>
-          </div>
-          <div>
-            <select ref={proficiencyRef} required className="select input-bordered w-full">
-              <option disabled selected value="">Select proficiency</option>
-              <option value="Beginner">Beginner</option>
-              <option value="Elementary">Elementary</option>
-              <option value="Intermediate">Intermediate</option>
-              <option value="Advanced">Advanced</option>
-              <option value="Proficient">Proficient</option>
-            </select>
+      <div className="collapse collapse-plus rounded-md bg-base-200">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">Add Langugae</div>
+        <div className="collapse-content">
 
-          </div>
+          <form onSubmit={_addLanguage} className="form-control">
+            {/* language */}
+            <div className="mb-3">
+              <div>
+                <label className="font-bold text-md">Language</label>
+              </div>
+              <div className="input input-bordered flex items-center gap-2 mt-1">
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder="Language"
+                  required
+                  ref={languageRef}
+                />
+              </div>
+            </div>
+            {/* proficiency */}
+            <div className="mb-3">
+              <div>
+                <label className="font-bold text-md">Proficiency</label>
+              </div>
+              <div>
+                <select ref={proficiencyRef} required className="select input-bordered w-full">
+                  <option disabled selected value="">Select proficiency</option>
+                  <option value="Beginner">Beginner</option>
+                  <option value="Elementary">Elementary</option>
+                  <option value="Intermediate">Intermediate</option>
+                  <option value="Advanced">Advanced</option>
+                  <option value="Proficient">Proficient</option>
+                </select>
+
+              </div>
+            </div>
+            {/* submit button */}
+            <div>
+              <button
+                className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <span className="loading loading-dots loading-xs"></span>
+                  </>
+                ) : (
+                  "Add"
+                )}
+              </button>
+            </div>
+          </form>
         </div>
-        {/* submit button */}
-        <div>
-          <button
-            className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
-            type="submit"
-            disabled={loading}
-          >
-            {loading ? (
-              <>
-                <span className="loading loading-dots loading-xs"></span>
-              </>
-            ) : (
-              "Add"
-            )}
-          </button>
-        </div>
-      </form>
+      </div>
     </>
   )
 }
