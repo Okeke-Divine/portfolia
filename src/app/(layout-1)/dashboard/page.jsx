e09c65db1e8f961fe382f1b15772351189b81335
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 01:07:50 +0100

    Updated

--- src/app/(layout-1)/dashboard/page.jsx
+++ src/app/(layout-1)/dashboard/page.jsx
@@ -1,6 +1,8 @@
 import DashboardTopSkeleton from "@/components/skeleton/dashboard/DashboardTopSkeleton";
 import { Suspense } from "react";
 import DashboardTopComponent from "@/components/dashboard/DashboardTopComponent";
+import prisma from "@/app/db";
+import { getUserId } from "@/utils/session";
 
 export const metadata = {
   title: "Dashboard",
@@ -9,6 +11,13 @@ export const metadata = {
 export const dynamic = "force-dynamic";
 
 const Dashboard = async () => {
+
+  const userId = await getUserId()
+
+  const userAnalytics = await prisma.UserAnalytics.findFirst({where: {
+    userId
+  }})
+
   return (
     <>
       <Suspense fallback={<DashboardTopSkeleton />}>
