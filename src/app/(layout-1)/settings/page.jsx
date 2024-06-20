a779bff0b6bce213b119607a9bfe9ae5ab4f521f
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 01:23:44 +0100

    Updated

--- src/app/(layout-1)/settings/page.jsx
+++ src/app/(layout-1)/settings/page.jsx
@@ -8,6 +8,11 @@ const SettingsPage = () => {
     return (
         <>
             <div>
+                <div className="text-sm breadcrumbs">
+                    <ul>
+                        <li>Settings </li>
+                    </ul>
+                </div>
                 <h1 className="flex items-center gap-2">
                     <i className="fi fi-tr-customization-cogwheel titleIcon"></i> Settings
                 </h1>
