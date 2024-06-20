4d517c84657048a751609b02c5347aa582f001f7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 01:15:39 +0100

    Updated

--- src/app/(layout-1)/settings/page.jsx
+++ src/app/(layout-1)/settings/page.jsx
@@ -1,7 +1,11 @@
+export const metadata = {
+    title: "Settings"
+}
+
 const SettingsPage = () => {
-  return (
-    <div>SettingsPage</div>
-  )
+    return (
+        <div>SettingsPage</div>
+    )
 }
 
 export default SettingsPage
