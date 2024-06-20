f7cb3bb5b6b2f38e10e57748d5f1c933cc7d5d24
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 15:38:03 +0100

    Updated

--- src/components/info/language/AddLanguage.jsx
+++ src/components/info/language/AddLanguage.jsx
@@ -6,13 +6,35 @@ const AddLanguage = () => {
   const languageRef = useRef(null)
   const proficiencyRef = useRef(null)
 
+
+  const [loading, setLoading] = useState(false)
+
   function _addLanguage(e) {
     e.preventDefault()
   }
 
   return (
     <>
-      <form onSubmit={_addLanguage} className="form-control"></form>
+      <form onSubmit={_addLanguage} className="form-control">
+
+
+        {/* submit button */}
+        <div>
+          <button
+            className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <span className="loading loading-dots loading-xs"></span>
+              </>
+            ) : (
+              "Add"
+            )}
+          </button>
+        </div>
+      </form>
     </>
   )
 }
