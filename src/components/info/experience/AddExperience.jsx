5de601b5c6b7a69f72b4acacaec2b3b67b8fa274
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 19:10:10 +0100

    Updated

--- src/components/info/experience/AddExperience.jsx
+++ src/components/info/experience/AddExperience.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+"use client"
+import { useRef, useState } from "react"
+import axios from "axios"
+import {
+  SweetAlertError,
+  SweetAlertSuccess,
+} from "@/utils/customSweetAlertFunction";
+import { generateYearsArray } from "@/utils/main";
 
 const AddExperience = () => {
+
+
+  const years = generateYearsArray();
+  const channel = new BroadcastChannel("user-certificates-channel")
+  const [loading, setLoading] = useState(false)
+
+  function _addExperience(e) {
+    e.preventDefault()
+    setLoading(true);
+  }
+
   return (
-    <div>AddExperience</div>
+    <>
+      <form onSubmit={_addExperience} className="form-control">
+
+      </form>
+    </>
   )
 }
 
