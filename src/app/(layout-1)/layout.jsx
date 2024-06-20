5acdb8d06bbec6af9c68e9d4a63bdd568db6828b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 01:21:13 +0100

    Updated

--- src/app/(layout-1)/layout.jsx
+++ src/app/(layout-1)/layout.jsx
@@ -10,7 +10,7 @@ const Layout1 = ({ children }) => {
         <div>
           <ApplicationSidedar />
         </div>
-        <div className="pl-0 md:pl-[250px] w-full">
+        <div className="pl-0 md:pl-[260px] w-full">
           <div>
             <ApplicationNavbar />
           </div>
