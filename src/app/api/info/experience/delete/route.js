5c4d8223102eee0bbbc6aebf95c410498dca4002
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 20:21:38 +0100

    Updated

---
+++ src/app/api/info/experience/delete/route.js
@@ -0,0 +1,26 @@
+import prisma from "@/app/db";
+import { badRequest, internalServerError, resourceDeleted } from "@/utils/prebuiltApiResponse";
+
+export const POST = async (req) => {
+    try {
+        const data = await req.json();
+        const { experience_id } = data;
+
+        if (!education_id || !parseInt(experience_id)) {
+            return badRequest("invalid experience_id")
+        }
+
+        const delete_experience = await prisma.UserExperience.delete({
+            where: {
+                id: experience_id
+            }
+        })
+        if (delete_experience) {
+            return resourceDeleted();
+        }
+
+        return internalServerError("failed to delete an education")
+    } catch (e) {
+        return internalServerError(e);
+    }
+};
