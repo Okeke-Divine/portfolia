47132d8d1e7ef0a7dc53d4ea2b1b15043053cc12
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 19:01:43 +0100

    Updated

--- src/components/info/education/AddEducation.jsx
+++ src/components/info/education/AddEducation.jsx
@@ -1,9 +1,45 @@
 "use client"
+import { useRef, useState } from "react"
+import axios from "axios"
+import {
+    SweetAlertError,
+    SweetAlertSuccess,
+} from "@/utils/customSweetAlertFunction";
 
 const AddEducation = () => {
-  return (
-    <div>AddEducation</div>
-  )
+
+
+    const channel = new BroadcastChannel("user-educations-channel")
+    const [loading, setLoading] = useState(false)
+
+
+    function _addEducation(e) {
+        e.preventDefault()
+        setLoading(true);
+    }
+
+    return (
+        <>
+            <form onSubmit={_addEducation} className="form-control">
+                {/* submit button */}
+                <div>
+                    <button
+                        className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+                        type="submit"
+                        disabled={loading}
+                    >
+                        {loading ? (
+                            <>
+                                <span className="loading loading-dots loading-xs"></span>
+                            </>
+                        ) : (
+                            "Add"
+                        )}
+                    </button>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default AddEducation
