c69d36d5dd269cb43f353b11e139379193525521
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 19:09:26 +0100

    Updated

--- src/app/(layout-1)/info/experience/page.jsx
+++ src/app/(layout-1)/info/experience/page.jsx
@@ -1,6 +1,17 @@
+import AddExperience from "@/components/info/experience/AddExperience"
+import ViewExperience from "@/components/info/experience/ViewExperience"
+
+export const metadata = {
+  title: "Experience | Info"
+}
+
 const ExperienceInfo = () => {
   return (
-    <div>ExperienceInfo</div>
+    <>
+      <AddExperience />
+      <div className="divider"></div>
+      <ViewExperience />
+    </>
   )
 }
 
