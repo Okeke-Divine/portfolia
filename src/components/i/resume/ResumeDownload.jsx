cd8648dbd9011c4d5cd944c0c289e06df744d805
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 01:33:17 +0100

    Updated

--- src/components/i/resume/ResumeDownload.jsx
+++ src/components/i/resume/ResumeDownload.jsx
@@ -3,7 +3,7 @@
 const ResumeDownload = () => {
     return (
         <>
-            <button className="fixed bottom-5 left-5 z-50">
+            <button className="fixed bottom-5 left-5 app-bg-primary hover:app-bg-primary-dark shadow-md hover:shadow-lg text-white duration-300 z-[100] rounded-full w-10 h-10 flex items-center justify-center">
                 <i class="fi fi-tr-cloud-download-alt text-4xl"></i>
             </button>
         </>
