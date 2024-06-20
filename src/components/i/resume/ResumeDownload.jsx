c6356412e7db18c38f0f04f4669a7afe912d9ba1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 02:03:46 +0100

    Updated

--- src/components/i/resume/ResumeDownload.jsx
+++ src/components/i/resume/ResumeDownload.jsx
@@ -4,29 +4,8 @@ import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 const ResumeDownload = ({ username }) => {
+
     const handleDownloadPdf = () => {
-        const element = document.getElementById('resumeContainer');
-        if (!element) {
-            console.error('Element with id "resumeContainer" not found.');
-            return;
-        }
-
-        // Use html2canvas to capture the content of the div as an image
-        html2canvas(element, { scale: 2 }).then(canvas => {
-            // Create a new jsPDF instance
-            const pdf = new jsPDF('p', 'mm', 'a4');
-
-            // Calculate dimensions to fit the content on the PDF
-            const imgWidth = 210; // A4 width in mm
-            const imgHeight = canvas.height * imgWidth / canvas.width;
-
-            // Add the captured image to the PDF
-            const imgData = canvas.toDataURL('image/png');
-            pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
-
-            // Save the PDF with a specific name
-            pdf.save(`${username}_resume.pdf`);
-        });
     };
 
     return (
