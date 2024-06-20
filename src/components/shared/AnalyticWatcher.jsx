48b08a7544cf8c4c78ed48392edbc242442d3f12
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 01:30:09 +0100

    Updated

--- src/components/shared/AnalyticWatcher.jsx
+++ src/components/shared/AnalyticWatcher.jsx
@@ -10,25 +10,38 @@ const AnalyticWatcher = ({ username, parent }) => {
         const handleClick = () => {
             if (parent == "portfolio") {
                 analysisTracker(username, 'portfolioClick')
-                console.log("sent")
             } else if (parent == "resume") {
-                console.log("sent")
                 analysisTracker(username, 'resumeClick');
             }
-            console.log(parent, "click");
+        };
+
+        const handleDownload = () => {
+         if (parent == "resume") {
+                analysisTracker(username, 'resumeDownload');
+            }
         };
 
         // Get all elements with the .clickMeCounter class
         const elements = document.querySelectorAll(".dmdomo");
+
+        const elements2 = document.querySelectorAll(".downsifn");
+
 
         // Add event listeners to all elements
         elements.forEach(element => {
             element.addEventListener("click", handleClick);
         });
 
+        elements2.forEach(element => {
+            element.addEventListener("click", handleDownload);
+        });
+
         return () => {
             elements.forEach(element => {
                 element.removeEventListener("click", handleClick);
+            });
+            elements2.forEach(element => {
+                element.removeEventListener("click", handleDownload);
             });
         }
     })
