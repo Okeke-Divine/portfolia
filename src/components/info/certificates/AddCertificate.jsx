d76044fdc919cd97fcee5509e727ef2beeb362b9
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 18:48:47 +0100

    Updated

--- src/components/info/certificates/AddCertificate.jsx
+++ src/components/info/certificates/AddCertificate.jsx
@@ -13,18 +13,33 @@ const AddCertificate = () => {
     const years = generateYearsArray();
     const channel = new BroadcastChannel("user-certificates-channel")
     const [loading, setLoading] = useState(false)
-  
 
-    function _addCertificate(e){
+
+    function _addCertificate(e) {
         e.preventDefault()
         setLoading(true);
     }
 
     return (
         <>
-      <form onSubmit={_addCertificate} className="form-control">
-        
-        </form>
+            <form onSubmit={_addCertificate} className="form-control">
+                {/* submit button */}
+                <div>
+                    <button
+                        className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+                        type="submit"
+                        disabled={loading}
+                    >
+                        {loading ? (
+                            <>
+                                <span className="loading loading-dots loading-xs"></span>
+                            </>
+                        ) : (
+                            "Add"
+                        )}
+                    </button>
+                </div>
+            </form>
         </>
     )
 }
