d5c7846bbd822550d85c80f6f01afe93b185e3d2
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-21 00:36:07 +0100

    Udated

--- src/components/i/resume/ResumeCertificates.jsx
+++ src/components/i/resume/ResumeCertificates.jsx
@@ -1,25 +1,25 @@
-const ResumeCertificates = async () => {
+const ResumeCertificates = async ({ userId }) => {
 
-    const certificates = await prisma.userLanguage.findMany({
+    const certificates = await prisma.userCertificate.findMany({
         where: { userId },
     });
 
 
-  return (
-    <>
-          <div className="app-resume-padding app-resume-container-margin-top">
-                        <div className="divider divider-start">
-                            <h3 className="uppercase">Certificates</h3>
+    return (
+        <>
+            <div className="app-resume-padding app-resume-container-margin-top">
+                <div className="divider divider-start">
+                    <h3 className="uppercase">Certificates</h3>
+                </div>
+                <div className="flex flex-wrap gap-x-2 md:gap-x-3 gap-y-2">
+                    {certificates.map((certificate, index) => (
+                        <div key={index} className="badge badge-outline badge-md">
+                            {certificate.name} - {certificate.issuer}
                         </div>
-                        <div className="flex flex-wrap gap-x-2 md:gap-x-3 gap-y-2">
-                            {certificates.map((certificate, index) => (
-                                <div key={index} className="badge badge-outline badge-md">
-                                    {language.name} - {language.proficiency}
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-    </>
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
 
