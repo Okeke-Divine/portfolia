31552ea490aa653f9800f605af56b1597f9113cc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 20:14:32 +0100

    Updated

--- src/components/info/education/ViewEducations.jsx
+++ src/components/info/education/ViewEducations.jsx
@@ -1,6 +1,38 @@
-"use client"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { app_color_primary } from "@/constants/shared/color";
+import Swal from "sweetalert2";
+import SkillSkeleton from "@/components/skeleton/skills/SkillSkeleton";
 
 const ViewEducations = () => {
+
+  const [educations, setEducations] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const channel = new BroadcastChannel("user-educations-channel");
+
+  useEffect(() => {
+    channel.onmessage = (event) => {
+      setLoading(true);
+      const newEducation = event.data.data;
+      setEducations((prevEducations) => [...prevEducations, newEducation]);
+      setLoading(false);
+    };
+
+    return () => {
+      channel.close();
+    };
+  }, [channel]);
+
+
+  useEffect(function () {
+    setLoading(true);
+    axios.get("/api/info/education").then((response) => {
+      setEducations(response.data.data);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div>ViewEducations</div>
   )
