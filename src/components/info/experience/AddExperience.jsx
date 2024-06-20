85d974d79cac27aa74b88c388d368a57b2272174
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-20 20:38:37 +0100

    Updated

--- src/components/info/experience/AddExperience.jsx
+++ src/components/info/experience/AddExperience.jsx
@@ -81,127 +81,133 @@ const AddExperience = () => {
 
   return (
     <>
-      <form onSubmit={_addExperience} className="form-control">
-        {/* language */}
-        <div className="mb-3">
-          <div>
-            <label className="font-bold text-md">Position</label>
-          </div>
-          <div className="input input-bordered flex items-center gap-2 mt-1">
-            <input
-              type="text"
-              className="grow"
-              placeholder="Position"
-              required
-              ref={positionRef}
-            />
-          </div>
-        </div>
-        {/* company */}
-        <div className="mb-3">
-          <div>
-            <label className="font-bold text-md">Company</label>
-          </div>
-          <div className="input input-bordered flex items-center gap-2 mt-1">
-            <input
-              type="text"
-              className="grow"
-              placeholder="Company"
-              required
-              ref={companyRef}
-            />
-          </div>
-        </div>
-        {/* start info */}
-        <div className="grid gap-2 grid-cols-1 md:grid-cols-2 mb-3">
-          {/* start month */}
-          <div className="mb-3">
+      <div className="collapse collapse-plus rounded-md bg-base-200">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">Add Experience</div>
+        <div className="collapse-content">
+          <form onSubmit={_addExperience} className="form-control">
+            {/* language */}
+            <div className="mb-3">
+              <div>
+                <label className="font-bold text-md">Position</label>
+              </div>
+              <div className="input input-bordered flex items-center gap-2 mt-1">
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder="Position"
+                  required
+                  ref={positionRef}
+                />
+              </div>
+            </div>
+            {/* company */}
+            <div className="mb-3">
+              <div>
+                <label className="font-bold text-md">Company</label>
+              </div>
+              <div className="input input-bordered flex items-center gap-2 mt-1">
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder="Company"
+                  required
+                  ref={companyRef}
+                />
+              </div>
+            </div>
+            {/* start info */}
+            <div className="grid gap-2 grid-cols-1 md:grid-cols-2 mb-3">
+              {/* start month */}
+              <div className="mb-3">
+                <div>
+                  <label className="font-bold text-md">Start Month</label>
+                </div>
+                <select ref={startMonthRef} defaultValue="" required className="select input-bordered w-full">
+                  <option disabled value="">Select Month</option>
+                  {months.map((month, index) => (
+                    <option key={index} value={month}>{month}</option>
+                  ))}
+                </select>
+              </div>
+              {/* start year */}
+              <div className="mb-3">
+                <div>
+                  <label className="font-bold text-md">Start Year</label>
+                </div>
+                <select ref={startYearRef} defaultValue="" required className="select input-bordered w-full">
+                  <option disabled value="">Select year</option>
+                  {years.map((year, index) => (
+                    <option key={index} value={year}>{year}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            {/* end start info */}
+            {/* end info */}
+            <div className="grid gap-2 grid-cols-1 md:grid-cols-2 mb-3">
+              {/* start month */}
+              <div className="mb-3">
+                <div>
+                  <label className="font-bold text-md">End Month (optional)</label>
+                </div>
+                <select ref={endMonthRef} defaultValue="" className="select input-bordered w-full">
+                  <option disabled value="" >Select Month</option>
+                  {months.map((month, index) => (
+                    <option key={index} value={month}>{month}</option>
+                  ))}
+                </select>
+              </div>
+              {/* start year */}
+              <div className="mb-3">
+                <div>
+                  <label className="font-bold text-md">End Year (optional)</label>
+                </div>
+                <div className="input input-bordered flex items-center gap-2 mt-1">
+                  <input
+                    type="number"
+                    className="grow"
+                    placeholder="End Year"
+                    ref={endYearRef}
+                  />
+                </div>
+              </div>
+            </div>
+            {/* end end info */}
+            {/* Description */}
+            <div className="mb-3">
+              <div>
+                <label htmlFor="about" className="font-bold text-md">
+                  Description
+                </label>
+              </div>
+              <textarea
+                rows={5}
+                required
+                ref={descriptionRef}
+                placeholder="Description"
+                className="textarea input-bordered w-full"
+              ></textarea>
+            </div>
+            {/* submit button */}
             <div>
-              <label className="font-bold text-md">Start Month</label>
+              <button
+                className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <span className="loading loading-dots loading-xs"></span>
+                  </>
+                ) : (
+                  "Add"
+                )}
+              </button>
             </div>
-            <select ref={startMonthRef} defaultValue="" required className="select input-bordered w-full">
-              <option disabled value="">Select Month</option>
-              {months.map((month, index) => (
-                <option key={index} value={month}>{month}</option>
-              ))}
-            </select>
-          </div>
-          {/* start year */}
-          <div className="mb-3">
-            <div>
-              <label className="font-bold text-md">Start Year</label>
-            </div>
-            <select ref={startYearRef} defaultValue="" required className="select input-bordered w-full">
-              <option disabled value="">Select year</option>
-              {years.map((year, index) => (
-                <option key={index} value={year}>{year}</option>
-              ))}
-            </select>
-          </div>
+          </form>
         </div>
-        {/* end start info */}
-        {/* end info */}
-        <div className="grid gap-2 grid-cols-1 md:grid-cols-2 mb-3">
-          {/* start month */}
-          <div className="mb-3">
-            <div>
-              <label className="font-bold text-md">End Month (optional)</label>
-            </div>
-            <select ref={endMonthRef} defaultValue="" className="select input-bordered w-full">
-              <option disabled value="" >Select Month</option>
-              {months.map((month, index) => (
-                <option key={index} value={month}>{month}</option>
-              ))}
-            </select>
-          </div>
-          {/* start year */}
-          <div className="mb-3">
-            <div>
-              <label className="font-bold text-md">End Year (optional)</label>
-            </div>
-            <div className="input input-bordered flex items-center gap-2 mt-1">
-              <input
-                type="number"
-                className="grow"
-                placeholder="End Year"
-                ref={endYearRef}
-              />
-            </div>
-          </div>
-        </div>
-        {/* end end info */}
-        {/* Description */}
-        <div className="mb-3">
-          <div>
-            <label htmlFor="about" className="font-bold text-md">
-              Description
-            </label>
-          </div>
-          <textarea
-            rows={5}
-            required
-            ref={descriptionRef}
-            placeholder="Description"
-            className="textarea input-bordered w-full"
-          ></textarea>
-        </div>
-        {/* submit button */}
-        <div>
-          <button
-            className="w-full app-bg-primary duration-300 hover:app-bg-primary-dark btn btn-md text-white"
-            type="submit"
-            disabled={loading}
-          >
-            {loading ? (
-              <>
-                <span className="loading loading-dots loading-xs"></span>
-              </>
-            ) : (
-              "Add"
-            )}
-          </button>
-        </div>
-      </form>
+      </div>
     </>
   )
 }
