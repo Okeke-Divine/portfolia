b42a43589a21bc7db4b40450ded9bac9cac4dce3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-21 03:14:58 +0100

    Updated

--- src/app/sitemap/portfolio/sitemap.js
+++ src/app/sitemap/portfolio/sitemap.js
@@ -1,19 +1,22 @@
+import prisma from "@/app/db";
 import { NEXT_PUBLIC_BASE_URL } from "@/constants/shared/constant";
 
 
 const changeFrequency = "daily";
 
+const currentTime = new Date().toISOString();
+
+
 export default async function sitemap() {
     const posts = await prisma.user.findMany({
         select: {
             username: true,
-            createdAt: true
         },
     });
 
-    const users = posts.map(({ slug, createdAt }) => ({
+    const users = posts.map(({ username }) => ({
         url: `${NEXT_PUBLIC_BASE_URL}/i/` + username,
-        lastModified: createdAt,
+        lastModified: currentTime,
         changeFrequency: changeFrequency,
     }));
 
