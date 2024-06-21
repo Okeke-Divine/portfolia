f93590bfd435553e9c1e0e76caf99f0f8f7cd028
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-21 16:38:35 +0100

    Uppdated

--- src/components/i/portfolio/PortfolioProjects.jsx
+++ src/components/i/portfolio/PortfolioProjects.jsx
@@ -20,71 +20,74 @@ const PortfolioProjects = async ({ user }) => {
   // const projects = [{}, {}, {}, {}, {}];
   return (
     <>
-      <section className="app-portfolio-padding" id="projects">
-        <div>
-          <div className="app-text-light-2 uppercase font-semibold tracking-wide text-sm text-center mb-1">
-            Portfolio
+      {projects.length > 0 ? (<>
+        <section className="app-portfolio-padding" id="projects">
+          <div>
+            <div className="app-text-light-2 uppercase font-semibold tracking-wide text-sm text-center mb-1">
+              Portfolio
+            </div>
+            <h1 className="text-center text-4xl">My Creative Works</h1>
           </div>
-          <h1 className="text-center text-4xl">My Creative Works</h1>
-        </div>
-        <div>
-          <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 mt-3">
-            {/* <div className="columns-1 gap-5 lg:gap-8 md:columns-2 lg:columns-3 space-y-5"> */}
-            {projects.map((project, index) => (
-              <div className="card shadow-xl" key={index}>
-                <figure class="w-full h-[250px] md:h-[200px] overflow-hidden">
-                  <img
-                    src={project.imageUrl != "" ? project.imageUrl : imageThumbnail}
-                    alt={
-                      project.title +
-                      " - " +
-                      project.description +
-                      " | " +
-                      user.fullname
-                    }
-                    class="object-cover w-full h-full"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{project.title}</h2>
-                  <p className="overflow-y-auto whitespace-pre-line">
-                    {project.description}
-                  </p>
-                  {project.url != "" ? (
-                    <>
-                      <div className="card-actions justify-end">
-                        <Link
-                          href={project.url}
-                          target="_blank"
-                          className="app-primary-button dmdomo"
-                        >
-                          View
-                        </Link>
-                      </div>
-                    </>
-                  ) : (
-                    ""
-                  )}
-                  {project.tags && project.tags !== "" ? (
-                    <>
-                      <div className="divider"></div>
-                      <div className="card-actions">
-                        {project.tags.split(",").map((tag, index) => (
-                          <div key={index} className="badge badge-outline">
-                            {tag.trim()}
-                          </div>
-                        ))}
-                      </div>
-                    </>
-                  ) : (
-                    ""
-                  )}
+          <div>
+            <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 mt-3">
+              {/* <div className="columns-1 gap-5 lg:gap-8 md:columns-2 lg:columns-3 space-y-5"> */}
+              {projects.map((project, index) => (
+                <div className="card shadow-xl" key={index}>
+                  <figure class="w-full h-[250px] md:h-[200px] overflow-hidden">
+                    <img
+                      src={project.imageUrl != "" ? project.imageUrl : imageThumbnail}
+                      alt={
+                        project.title +
+                        " - " +
+                        project.description +
+                        " | " +
+                        user.fullname
+                      }
+                      class="object-cover w-full h-full"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{project.title}</h2>
+                    <p className="overflow-y-auto whitespace-pre-line">
+                      {project.description}
+                    </p>
+                    {project.url != "" ? (
+                      <>
+                        <div className="card-actions justify-end">
+                          <Link
+                            href={project.url}
+                            target="_blank"
+                            className="app-primary-button dmdomo"
+                          >
+                            View
+                          </Link>
+                        </div>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                    {project.tags && project.tags !== "" ? (
+                      <>
+                        <div className="divider"></div>
+                        <div className="card-actions">
+                          {project.tags.split(",").map((tag, index) => (
+                            <div key={index} className="badge badge-outline">
+                              {tag.trim()}
+                            </div>
+                          ))}
+                        </div>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </>) : ""}
+
     </>
   );
 };
