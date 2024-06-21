46d144ca2158fd866305758b004720e511765d02
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-21 16:31:30 +0100

    Updated

--- src/components/i/resume/ResumeCertificates.jsx
+++ src/components/i/resume/ResumeCertificates.jsx
@@ -9,19 +9,21 @@ const ResumeCertificates = async ({ userId }) => {
 
     return (
         <>
-            <div className="app-resume-padding app-resume-container-margin-top">
-                <div className="divider divider-start">
-                    <h3 className="uppercase">Certificates</h3>
-                </div>
-                <div className="">
-                    {certificates.map((certificate, index) => (
-                        <div key={index} className="mb-2">
-                            <div><b> {certificate.name}</b> - {certificate.issuer}</div>
-                            <div className="text-gray-600 italic">{certificate.issueMonth} {certificate.issueYear}</div>
-                        </div>
-                    ))}
-                </div>
-            </div>
+            {certificates.length > 0 ? (<>
+                <div className="app-resume-padding app-resume-container-margin-top">
+                    <div className="divider divider-start">
+                        <h3 className="uppercase">Certificates</h3>
+                    </div>
+                    <div className="">
+                        {certificates.map((certificate, index) => (
+                            <div key={index} className="mb-2">
+                                <div><b> {certificate.name}</b> - {certificate.issuer}</div>
+                                <div className="text-gray-600 italic">{certificate.issueMonth} {certificate.issueYear}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div></>) : ""}
+
         </>
     )
 }
