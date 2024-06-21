df2b9e9ef5a5605fb0c30572d64544fec43ef773
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-21 01:27:18 +0100

    UUpdated

--- src/components/landing-page/LowerCallToAction.jsx
+++ src/components/landing-page/LowerCallToAction.jsx
@@ -1,5 +1,11 @@
 
 const LowerCallToAction = () => {
+
+  function usernameRedirect(data){
+    "use server"
+    console.log(data)
+  }
+
   return (
     <>
       <div className="bg-base-300 py-5 md:py-10">
@@ -7,8 +13,8 @@ const LowerCallToAction = () => {
           <div className="text-center font-bold text-3xl">Launch your portfolio today</div>
           <div className="py-3 text-center">Just fill in your info and your portfolio + resume will be automatically generated</div>
           <div className="join flex justify-center mt-1">
-            <form>
-              <input type="text" placeholder="Your username" className="join-item input input-bordered" />
+            <form action={usernameRedirect}>
+              <input name="username" id="username" type="text" placeholder="Your username" className="join-item input input-bordered" />
               <button className="join-item btn app-button-primary app-primary-button w-fit">Create my Portfolio</button>
             </form>
           </div>
