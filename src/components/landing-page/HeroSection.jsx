c4bc2472eee1d6f4ab6e105864e83bff743d972c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-22 02:12:46 +0100

    Updated

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
                 <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                 <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                 <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked={true} />
               </div>
             </div>
           </div>
