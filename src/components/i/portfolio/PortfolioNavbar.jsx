64d03cb1c991d6ddf6ed3b068929d4ca2de5d556
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-22 18:13:08 +0100

    Updated

--- src/components/i/portfolio/PortfolioNavbar.jsx
+++ src/components/i/portfolio/PortfolioNavbar.jsx
@@ -5,7 +5,7 @@ import PortfolioSocialLink from "./PortfolioSocialLink";
 const PortfolioNavbar = ({ userId, profilePicture_url }) => {
   return (
     <>
-      <div className="navbar px-5 md:px-10 py-5 md:py-5 bg-base-200 fixed top-0 left-0 z-50">
+      <div className="navbar px-5 md:px-10 py-5 md:py-5 bg-base-200 fixed top-0 left-0 z-50 border-b-2">
         <div className="navbar-start">
           <div className="avatar cursor-pointer">
             <div className="w-10 rounded-full ring ring-green-300">
