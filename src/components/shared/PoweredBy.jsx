fc2694e1a32aa4c277a7791cc1ec9d77a87b5ca0
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-22 18:21:17 +0100

    UpDATED

--- src/components/shared/PoweredBy.jsx
+++ src/components/shared/PoweredBy.jsx
@@ -1,11 +1,10 @@
-"use client"
 import config from "@/data/config.json"
-import Link from "next/link"
+// import Link from "next/link"
 
 const PoweredBy = () => {
     return (
         <>
-            <div className="pr-2 w-[0px] duration-300 hidden group-hover:w-full  group-hover:block">Powered  by <Link className="font-bold" href="/" target="_blank"> {config.app_name}</Link> </div>
+            <div className="pr-2 w-[0px] duration-300 hidden group-hover:w-full  group-hover:block">Powered  by <span className="font-bold"> {config.app_name}</span> </div>
         </>
     )
 }
