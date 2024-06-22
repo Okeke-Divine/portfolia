44680af5d39dbb785de51fbb448146ad5c839258
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-22 18:32:41 +0100

    Updated

--- src/components/shared/PoweredBy.jsx
+++ src/components/shared/PoweredBy.jsx
@@ -14,7 +14,7 @@ const PoweredBy = ({ variation }) => {
                             alt={config.app_name + "'s logo"}
                         />
                     </div>
-                    <div className="pr-2 w-[0px] duration-300 hidden group-hover:w-full  group-hover:block">Powered  by <span className="font-bold"> {config.app_name}</span> </div>
+                    <div className="pr-2 w-[0px] md:w-full duration-300 hidden group-hover:w-full  group-hover:block">Powered  by <span className="font-bold"> {config.app_name}</span> </div>
                 </div>
             </Link>
         </>
