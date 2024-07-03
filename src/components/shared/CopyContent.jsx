5d57a95ced8a754fed075a880e43f0bb1ecf4a48
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-07-03 14:50:46 +0100

    Updated

--- src/components/shared/CopyContent.jsx
+++ src/components/shared/CopyContent.jsx
@@ -1,20 +1,41 @@
 "use client"
+import { useState } from 'react';
+import Link from 'next/link';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Swal from 'sweetalert2';
 
 const CopyContent = ({ url }) => {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        setCopied(true);
+        Swal.fire({
+            icon: 'success',
+            title: 'Copied!',
+            text: 'URL copied to clipboard',
+            timer: 1500,
+            showConfirmButton: false
+        });
+    };
+
     return (
         <>
             <div className="input border-2 border-gray-100 flex items-center gap-2 mt-1">
                 <input
                     value={url}
                     required
-                    disabled
                     readOnly
                     className="grow"
                 />
-                <i className="fi fi-tr-clone flaticon-offset"></i>
-                      <i className="fi fi-tr-arrow-up-right-from-square flaticon-offset"></i>
-            </div></>
-    )
-}
+                <CopyToClipboard text={url} onCopy={handleCopy}>
+                    <i className="fi fi-tr-clone flaticon-offset cursor-pointer"></i>
+                </CopyToClipboard>
+                <Link href={url} target="_blank">
+                    <i className="fi fi-tr-arrow-up-right-from-square flaticon-offset cursor-pointer"></i>
+                </Link>
+            </div>
+        </>
+    );
+};
 
-export default CopyContent
+export default CopyContent;
