591cb8226b3c2b54cd3a769c77d5018f8c4e3d13
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-07-07 20:18:10 +0100

    Updated

--- src/app/(layout-1)/resume/builder/page.jsx
+++ src/app/(layout-1)/resume/builder/page.jsx
@@ -11,7 +11,7 @@ const ResumeBuilder = async () => {
 
   const userId = await getUserId('force');
   const _resumeIsViewable = await retrieveUserConfig('resumeIsViewable', { id_type: 'id', id_value: userId });
-  const resumeIsViewable = (_resumeIsViewable == null) ? false : (_resumeIsViewable == "true") ? true : false;
+  const resumeIsViewable = (_resumeIsViewable == null) ? true : (_resumeIsViewable == "true") ? true : false;
   console.log("resumeIsViewable: ", resumeIsViewable);
 
   return (
