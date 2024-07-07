7a7918b3ee2e5714333be5232f0899d4175783bd
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-07-07 16:28:07 +0100

    Updated

--- src/app/forgot-password/page.jsx
+++ src/app/forgot-password/page.jsx
@@ -1,3 +1,4 @@
+import config from "@/data/config.json";
 export const metadata = {
     title: "Forgot Password"
 }
@@ -8,7 +9,14 @@ const ForgotPassword = () => {
 
             <div className="bg-base-200 flex justify-center items-center min-h-[100vh]">
                 <div className="w-[80%] md:w-[350px] bg-white p-5 md:p-10 rounded-lg shadow-sm">
-                    Gt
+                    <div className="flex justify-center">
+                        <img
+                            className="w-24 mb-2 rounded-full shadow-lg"
+                            src="/images/logo/logo.png"
+                            alt={config.app_name + "'s logo"}
+                        />
+                    </div>
+                    <h1>Reset Password!</h1>
                 </div>
             </div>
         </>
