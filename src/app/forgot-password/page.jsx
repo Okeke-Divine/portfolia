788ff2d9335025ee84afcf0c6aff2a4327daf049
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-07-07 16:27:25 +0100

    Udated

--- src/app/forgot-password/page.jsx
+++ src/app/forgot-password/page.jsx
@@ -6,8 +6,8 @@ const ForgotPassword = () => {
     return (
         <>
 
-            <div className="bg-base-200 justify-center items-center min-h-[100vh]">
-                <div className="w-[80%] md:w-[350px]">
+            <div className="bg-base-200 flex justify-center items-center min-h-[100vh]">
+                <div className="w-[80%] md:w-[350px] bg-white p-5 md:p-10 rounded-lg shadow-sm">
                     Gt
                 </div>
             </div>
